70520a8171161efde1c7f429b3a70981f35bd793
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-07-12 18:05:54 -0400

    Set up connection with Supabase

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
 from supabase_py import create_client
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()
 
-app.run()
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+@app.route('/')
+def test():
+    result = supabase.table('data_sources').select("*").execute()
+    data = result.get('data', [])
+    return data
+
+if __name__ == '__main__':
+    app.run()
