442d665db71771c6c487acbc15323ee2533e7c48
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-07-14 17:31:43 -0400

    Set up initial quick search and queried for county matches

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-@app.route('/')
-def test():
-    result = supabase.table('data_sources').select("*").execute()
-    data = result.get('data', [])
-    return data
+@app.route('/quick-search/<search>/<county>')
+def quick_search(search, county):
+    # Query for all county_fips codes that match the county name searched
+    counties = supabase.table('counties').select('fips').eq('name', county).execute()
+    counties_fips = counties.get('data', []) 
+
+    return counties_fips
 
 if __name__ == '__main__':
     app.run()
