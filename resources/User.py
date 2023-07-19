31cfc60ca4f0294fa09745a6c9744594029a0fc8
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-07-19 16:16:13 -0400

    Set up login functionality

--- resources/User.py
+++ resources/User.py
@@ -1,7 +1,21 @@
 from flask_restful import Resource
 from flask import request
+from flask_jwt import jwt_required
+import jwt
+import os
 
 class User(Resource):
+    def get(self):
+        try:
+            email = request.get_json('email')
+            password = request.get_json('password')
+            user = self.supabase.table('users').select('*').eq('email', email).execute()['data']
+            SECRET_KEY = os.getenv('SECRET_KEY')
+            if self.bcrypt.check_password_hash(user.password_digest, password):
+                token = jwt.encode({'payload': user}, SECRET_KEY, algorithm="HS256")
+                return token
+        except Exception as e:
+            return {'error': e}
     
     def post(self):
         try:
