c13d34e11f98746317e376b710fc49db45e78fff
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-07-19 14:22:17 -0400

    Added error handling to quick_search

--- app.py
+++ app.py
@@ -27,35 +27,39 @@ def initialize_supabase_client():
 
 supabase = initialize_supabase_client()
 
-
 @app.route('/quick-search/<search>/<county>')
 def quick_search(search, county):
-    # Query for all county_fips codes that match the county name searched
-    counties = supabase.table('counties').select('fips').eq('name', county).execute()
-    counties_fips = counties.get('data', []) 
+    try:
+        data_sources = {'count': 0, 'data': []}
 
-    data_sources = {'count': 0, 'data': []}
+        # Query for all county_fips codes that match the county name searched
+        counties = supabase.table('counties').select('fips').eq('name', county).execute()
+        counties_fips = counties.get('data', []) 
 
-    if len(counties_fips) > 0:
-        # For each county_fip code, query for all agencies within that county and add to all agency list
-        all_agencies = []
-        for county_fips in counties_fips:
-            fips = str(county_fips['fips'])
-            agencies = supabase.table('agencies').select('name, municipality, state_iso, airtable_uid').eq('county_fips', fips).execute()
-            agencies_data = agencies.get('data', [])
-            for agency_data in agencies_data:
-                all_agencies.append(agency_data)
+        if len(counties_fips) > 0:
+            # For each county_fip code, query for all agencies within that county and add to all agency list
+            all_agencies = []
+            for county_fips in counties_fips:
+                fips = str(county_fips['fips'])
+                agencies = supabase.table('agencies').select('name, municipality, state_iso, airtable_uid').eq('county_fips', fips).execute()
+                agencies_data = agencies.get('data', [])
+                for agency_data in agencies_data:
+                    all_agencies.append(agency_data)
+            
+            # For each agency_uid, find all matches in the data_sources table that also have a partial match with the search term
+            for agency in all_agencies:
+                agency_data_sources = supabase.table('data_sources').select('name, description, record_type, source_url, record_format, coverage_start, coverage_end, agency_supplied').ilike('name', f"%{search}%").eq('agency_described', f"['{agency['airtable_uid']}']").execute()
+                agency_data_sources_records = agency_data_sources.get('data')
+                for record in agency_data_sources_records:
+                    data_sources['count'] += 1
+                    data_sources['data'].append({**record, **agency})
+            return data_sources
+
+        else:
+            return data_sources
         
-        # For each agency_uid, find all matches in the data_sources table that also have a partial match with the search term
-        for agency in all_agencies:
-            agency_data_sources = supabase.table('data_sources').select('name, description, record_type, source_url, record_format, coverage_start, coverage_end, agency_supplied').ilike('name', f"%{search}%").eq('agency_described', f"['{agency['airtable_uid']}']").execute()
-            agency_data_sources_records = agency_data_sources.get('data')
-            for record in agency_data_sources_records:
-                data_sources['count'] += 1
-                data_sources['data'].append({**record, **agency})
-        return data_sources
-
-    else:
+    except:
+        print('Error during quick search operation')
         return data_sources
 
 if __name__ == '__main__':
