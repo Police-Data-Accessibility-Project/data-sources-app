bf255e6142581ad0b29418df98aa02cd5a251769
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-07-21 17:50:30 -0400

    Fixed user/post resource to write on Supabase user table

--- resources/User.py
+++ resources/User.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from flask import request
+from flask import request, jsonify
 from flask_jwt import jwt_required
 import jwt
 import os
@@ -14,11 +14,11 @@ class User(Resource):
             data = request.get_json()
             email = data.get('email')
             password = data.get('password')
-            user = self.supabase.table('users').select('*').eq('email', email).execute()['data']
+            user = self.supabase.table('users').select('*').eq('email', email).execute()['data'][0]
             SECRET_KEY = os.getenv('SECRET_KEY')
-            if self.bcrypt.check_password_hash(user.password_digest, password):
+            if self.bcrypt.check_password_hash(user['password_digest'], password):
                 token = jwt.encode({'payload': user}, SECRET_KEY, algorithm="HS256")
-                return token
+                return jsonify(token)
         except Exception as e:
             return {'error': e}
     
@@ -28,7 +28,7 @@ class User(Resource):
             email = data.get('email')
             password = data.get('password')
             password_digest = self.bcrypt.generate_password_hash(password)
-            user = self.supabase.table('users').insert({"id": 1, "email": email, "password_digest": password_digest}).execute()
+            user = self.supabase.table('users').insert({"email": email, "password_digest": str(password_digest)}).execute()
             return user
         except Exception as e:
             return {'error': e}
