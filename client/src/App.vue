55d844d4b55bbaf5a8f366706d374f47af0a7a92
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-07-26 13:21:06 -0400

    Removed template filler

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hi</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
