dc7e52bd0c0208aecbe7dcb298e0d781a9b412e9
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-07-27 14:23:29 -0400

    Added in links to footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,12 @@
   <img src="@/assets/logo.svg" />
   <h2>Our mission is to help people locate, understand, and share public records about every U.S. police system. Try giving our database a search to see if we can help you find public records.</h2>
   <QuickSearchForm :searchTerm="searchTerm" :county="county" @handleChange="handleChange" @handleSubmit="handleSubmit"/>
+  <button @click="console.log('Clicked advanced search')">Advanced search</button>
+  <a href="https://airtable.com/shrbFfWk6fjzGnNsk" target="_blank" rel="noopener noreferrer">Request data</a>
+  <a href="https://pdap.io/index.html" target="_blank" rel="noopener noreferrer">Ask a question</a>
+  <a href="https://airtable.com/shrJafakrcmTxHU2i" target="_blank" rel="noopener noreferrer">Submit a Data Source</a>
+  <a href="https://pdap.io/index.html" target="_blank" rel="noopener noreferrer">Report issue</a>
+  <a href="https://docs.pdap.io/" target="_blank" rel="noopener noreferrer">Docs</a>
 </template>
 
 <script>
