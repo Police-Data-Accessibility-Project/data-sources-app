d57251ba5522280a4bb5ec6036fd528a910b8f54
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-07-27 14:03:52 -0400

    Added QuickSearchForm to main page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,41 @@
 <template>
-  <h1>Hi</h1>
+  <img src="@/assets/logo.svg" />
+  <h2>Our mission is to help people locate, understand, and share public records about every U.S. police system. Try giving our database a search to see if we can help you find public records.</h2>
+  <QuickSearchForm :searchTerm="searchTerm" :county="county" @handleChange="handleChange" @handleSubmit="handleSubmit"/>
 </template>
 
 <script>
+import QuickSearchForm from './components/QuickSearchForm.vue';
+import {BASE_URL} from '../globals'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    
+    QuickSearchForm
+  },
+  data: () => ({
+    searchTerm: '',
+    county: '',
+    searchResult: ''
+  }),
+  methods: {
+    handleChange(name, value) {
+      this[name] = value
+    },
+    async handleSubmit() {
+      const res = await axios.get(`${BASE_URL}/quick-search/${this.searchTerm}/${this.county}`)
+      this.searchResult = res.data
+      this.searchTerm = ''
+      this.county = ''
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ client/src/components/QuickSearchForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form @submit="handleSubmit">
+      <label for="searchTerm">What are you looking for?</label>
+      <input @input="handleChange" placeholder="Complaints" :value="searchTerm" name="searchTerm" />
+      <label for="county">From where?</label>
+      <input @input="handleChange" placeholder="Allegheny County" :value="county" name="county" />
+      <button>Search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuickSearchForm',
+  props: {
+    searchTerm: String,
+    county: String
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('handleChange', e.target.name, e.target.value)
+    },
+    handleSubmit(e) {
+      e.preventDefault()
+      this.$emit('handleSubmit')
+    }
+  }
+}
+</script>
