b33951338859d1b0933a0124cecd1bb5889faca1
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-08-02 13:56:29 -0400

    Created standalone results page with searchResult data at this level

---
+++ client/src/pages/SearchResultPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <h2>Search results</h2>
+  <div v-if="!searched">
+    <p>Loading results...</p>
+  </div>
+  <div v-else>
+    <p>You searched "{{ searchTerm }}" for {{county}} County and you got {{ searchResult.count }} results</p>
+    <div v-if="searchResult.count > 0">
+      <SearchResultCard :key="dataSource.uuid" v-for="dataSource in searchResult?.data" :dataSource="dataSource"/>
+    </div>
+    <div v-else>
+      <p>No results found.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchResultCard from '../components/SearchResultCard.vue';
+import {BASE_URL} from '../../globals'
+import axios from 'axios'
+
+export default {
+  name: 'SearchResultPage',
+  components: {
+    SearchResultCard
+  },
+  data: () => ({
+    searched: false,
+    searchResult: {},
+    searchTerm: '',
+    county: ''
+  }),
+  mounted: function() {
+    this.searchTerm = this.$route.params.searchTerm
+    this.county = this.$route.params.county
+    this.search()
+  },
+  methods: {
+    async search() {
+      const res = await axios.get(`${BASE_URL}/quick-search/${this.searchTerm}/${this.county}`)
+      this.searchResult = res.data
+      this.searched = true
+    }
+  }
+}
+</script>
