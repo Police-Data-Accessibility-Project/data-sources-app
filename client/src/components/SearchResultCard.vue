42737035273147753d70cbf92fa1946ce0d09525
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-08-02 16:22:50 -0400

    Set up initial styling for search results page and card

--- client/src/components/SearchResultCard.vue
+++ client/src/components/SearchResultCard.vue
@@ -1,35 +1,39 @@
 <template>
-  <div class="search-result-card ">
-    <p>{{ dataSource.data_source_name }}</p>
-    <p v-if="dataSource.agency_name">{{ dataSource.agency_name }}</p>
-    <p v-else>Agency Unknown</p>
-    <p v-if="dataSource.municipality && dataSource.state_iso">{{ dataSource.municipality }}, {{ dataSource.state_iso }}</p>
-    <p v-else-if="dataSource.municipality">{{ dataSource.municipality }}</p>
-    <p v-else-if="dataSource.state_iso">{{ dataSource.state_iso }}</p>
-    <p v-else>Location Unknown</p>
-    <p v-if="dataSource.record_type">Record type</p>
-    <p v-if="dataSource.record_type">{{ dataSource.record_type }}</p>
-    <p v-else>Record Type Unknown</p>
-    <p v-if="dataSource.coverage_start || dataSource.coverage_end">Coverage</p>
-    <p v-if="dataSource.coverage_start && dataSource.coverage_end">{{ dataSource.coverage_start }}-{{ dataSource.coverage_end }}</p>
-    <p v-else-if="dataSource.coverage_start && !dataSource.coverage_end">
+  <div class="search-result-card small">
+    <div class="search-result-title">{{ dataSource.data_source_name }}</div>
+    <div class="search-result-agency">
+      <p v-if="dataSource.agency_name">{{ dataSource.agency_name }}</p>
+      <p v-else>Agency Unknown</p>
+    </div>
+    <div class="search-result-place">
+      <p v-if="dataSource.municipality && dataSource.state_iso">{{ dataSource.municipality }}, {{ dataSource.state_iso }}</p>
+      <p v-else-if="dataSource.municipality">{{ dataSource.municipality }}</p>
+      <p v-else-if="dataSource.state_iso">{{ dataSource.state_iso }}</p>
+      <p v-else>Location Unknown</p>
+    </div>
+    <p class="search-result-label" v-if="dataSource.record_type">Record type</p>
+    <p class="search-result-data" v-if="dataSource.record_type">{{ dataSource.record_type }}</p>
+    <p class="search-result-data" v-else>Record Type Unknown</p>
+    <p class="search-result-label" v-if="dataSource.coverage_start || dataSource.coverage_end">Coverage</p>
+    <p class="search-result-data" v-if="dataSource.coverage_start && dataSource.coverage_end">{{ dataSource.coverage_start }}-{{ dataSource.coverage_end }}</p>
+    <p class="search-result-data" v-else-if="dataSource.coverage_start && !dataSource.coverage_end">
       {{ dataSource.coverage_start }} - End Date Unknown
     </p>
-    <p v-else-if="!dataSource.coverage_start && dataSource.coverage_end">
+    <p class="search-result-data" v-else-if="!dataSource.coverage_start && dataSource.coverage_end">
       Start Date Unknown - {{ dataSource.coverage_end }}
     </p>
-    <p v-else>
+    <p class="search-result-data" v-else>
       Coverage Date Unknown
     </p>
+    <p class="search-result-label">Formats available</p>
     <div v-if="dataSource.record_formats">
-      <p>Formats available</p>
-      <p :key="recordFormat" v-for="recordFormat in dataSource.record_formats">
+      <p class="search-result-data" :key="recordFormat" v-for="recordFormat in dataSource.record_formats">
         {{ recordFormat }}
       </p>
     </div>
-    <p v-else>Data Formats Unknown</p>
-    <button @click="openSource" :href="dataSource.source_url">Visit Source URL</button>
-    <button @click="showDetails">Source Details</button>
+    <p class="search-result-data" v-else>Data Formats Unknown</p>
+    <button class="button" @click="openSource" :href="dataSource.source_url">Visit Source URL</button>
+    <button class="source button" @click="showDetails">Source Details</button>
     <p v-if="expand && dataSource.description">
       {{ dataSource.description }}
     </p>
@@ -58,5 +62,44 @@
 </script>
 
 <style>
+.search-result-card {
+  border: 1px solid black;
+  width: 25%;
+  min-width: 200px;
+  margin: 1rem;
+  padding: 1rem
+}
 
+.search-result-title {
+  font-weight: bold;
+}
+
+.search-result-agency p {
+  font-weight: 500;
+}
+
+.search-result-agency p, .search-result-place p {
+  font-size: 60%;
+  font-style: italic;
+  line-height: 1;
+}
+
+.search-result-label {
+  line-height: 1;
+  font-weight: 500;
+}
+
+.search-result-data {
+  line-height: 1;
+}
+
+.source {
+  background-color: #bfc0c0;
+}
+
+@media (prefers-color-scheme: dark) {
+  .search-result-card {
+    border: 1px solid white;
+  }
+}
 </style>
--- client/src/pages/SearchResultPage.vue
+++ client/src/pages/SearchResultPage.vue
@@ -1,16 +1,20 @@
 <template>
-  <h2>Search results</h2>
-  <div v-if="!searched">
-    <p>Loading results...</p>
-  </div>
-  <div v-else>
-    <p>You searched "{{ searchTerm }}" for {{county}} County and you got {{ searchResult.count }} results</p>
-    <button @click="openForm">Missing something? Request data here</button>
-    <div v-if="searchResult.count > 0">
-      <SearchResultCard :key="dataSource.uuid" v-for="dataSource in searchResult?.data" :dataSource="dataSource"/>
+  <div class="search-results-page">
+    <h2>Search results</h2>
+    <div class="loading-section" v-if="!searched">
+      <p>Loading results...</p>
     </div>
-    <div v-else>
-      <p>No results found.</p>
+    <div class="search-results-section" v-else>
+      <div class="search-results-section-header small">
+        <p>You searched "{{ searchTerm }}" in {{county}} County and you got {{ searchResult.count }} results</p>
+        <button class="button" @click="openForm">Missing something? Request data here</button>
+      </div>
+      <div class="search-results-content" v-if="searchResult.count > 0">
+        <SearchResultCard :key="dataSource.uuid" v-for="dataSource in searchResult?.data" :dataSource="dataSource"/>
+      </div>
+      <div v-else>
+        <p>No results found.</p>
+      </div>
     </div>
   </div>
 </template>
@@ -48,3 +52,49 @@ export default {
   }
 }
 </script>
+
+<style>
+.loading-section {
+  min-height: 75vh;
+  text-align: center;
+  margin: 2rem 0
+}
+
+.search-results-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column wrap;
+  min-height: 75vh;
+}
+
+.search-results-page h2 {
+  width: 75%;
+  min-width: 450px;
+}
+
+.search-results-section {
+  display: flex;
+  justify-content: center;
+  flex-flow: column wrap;
+  width: 75%;
+  min-width: 450px;
+}
+
+.search-results-section-header {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.search-results-section-header p {
+  margin-right: 2rem;
+}
+
+.search-results-content {
+  display: flex;
+  /* justify-content: space-between; */
+  flex-wrap: wrap;
+}
+
+</style>
