a005f8a48b682afe828acfef29055fc6c9e16acb
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-08-07 18:40:18 -0400

    Added testing for all elements in SearchResultCard

--- client/src/components/SearchResultCard.vue
+++ client/src/components/SearchResultCard.vue
@@ -1,43 +1,43 @@
 <template>
-  <div class="search-result-card small">
-    <div class="search-result-title">{{ dataSource.data_source_name }}</div>
-    <div class="search-result-agency">
-      <p v-if="dataSource.agency_name">{{ dataSource.agency_name }}</p>
-      <p v-else>Agency Unknown</p>
+  <div class="search-result-card small" data-test="search-result-card">
+    <div class="search-result-title" data-test="search-result-title">{{ dataSource.data_source_name }}</div>
+    <div class="search-result-agency" data-test="search-result-agency">
+      <p v-if="dataSource.agency_name" data-test="search-result-agency-known">{{ dataSource.agency_name }}</p>
+      <p v-else data-test="search-result-agency-unknown">Agency Unknown</p>
     </div>
-    <div class="search-result-place">
-      <p v-if="dataSource.municipality && dataSource.state_iso">{{ dataSource.municipality }}, {{ dataSource.state_iso }}</p>
-      <p v-else-if="dataSource.municipality">{{ dataSource.municipality }}</p>
-      <p v-else-if="dataSource.state_iso">{{ dataSource.state_iso }}</p>
-      <p v-else>Location Unknown</p>
+    <div class="search-result-place" data-test="search-result-place">
+      <p v-if="dataSource.municipality && dataSource.state_iso" data-test="search-result-place-state-municipality">{{ dataSource.municipality }}, {{ dataSource.state_iso }}</p>
+      <p v-else-if="dataSource.municipality" data-test="search-result-place-municipality">{{ dataSource.municipality }}</p>
+      <p v-else-if="dataSource.state_iso" data-test="search-result-place-state">{{ dataSource.state_iso }}</p>
+      <p v-else data-test="search-result-place-unknown">Location Unknown</p>
     </div>
-    <p class="search-result-label" v-if="dataSource.record_type">Record type</p>
-    <p class="search-result-data" v-if="dataSource.record_type">{{ dataSource.record_type }}</p>
-    <p class="search-result-data" v-else>Record Type Unknown</p>
-    <p class="search-result-label" v-if="dataSource.coverage_start || dataSource.coverage_end">Coverage</p>
-    <p class="search-result-data" v-if="dataSource.coverage_start && dataSource.coverage_end">{{ formatDate(dataSource.coverage_start) }}-{{ formatDate(dataSource.coverage_end) }}</p>
-    <p class="search-result-data" v-else-if="dataSource.coverage_start && !dataSource.coverage_end">
+    <p class="search-result-label" v-if="dataSource.record_type" data-test="search-result-record-label">Record type</p>
+    <p class="search-result-data" data-test="search-result-record-type" v-if="dataSource.record_type">{{ dataSource.record_type }}</p>
+    <p class="search-result-data" v-else data-test="search-result-record-type-unknown">Record Type Unknown</p>
+    <p class="search-result-label" data-test="search-result-label-coverage" v-if="dataSource.coverage_start || dataSource.coverage_end">Coverage</p>
+    <p class="search-result-data" v-if="dataSource.coverage_start && dataSource.coverage_end" data-test="search-result-coverage-start-end">{{ formatDate(dataSource.coverage_start) }}-{{ formatDate(dataSource.coverage_end) }}</p>
+    <p class="search-result-data" v-else-if="dataSource.coverage_start && !dataSource.coverage_end" data-test="search-result-coverage-start">
       {{ formatDate(dataSource.coverage_start) }} - End Date Unknown
     </p>
-    <p class="search-result-data" v-else-if="!dataSource.coverage_start && dataSource.coverage_end">
+    <p class="search-result-data" v-else-if="!dataSource.coverage_start && dataSource.coverage_end" data-test="search-result-coverage-end">
       Start Date Unknown - {{ formatDate(dataSource.coverage_end) }}
     </p>
-    <p class="search-result-data" v-else>
+    <p class="search-result-data" v-else data-test="search-result-coverage-unknown">
       Coverage Date Unknown
     </p>
-    <p class="search-result-label">Formats available</p>
-    <div v-if="dataSource.record_formats">
-      <p class="search-result-data" :key="recordFormat" v-for="recordFormat in dataSource.record_formats">
+    <p class="search-result-label" data-test="search-result-label-formats">Formats available</p>
+    <div v-if="dataSource.record_format" data-test="search-result-formats">
+      <p class="search-result-data" :key="recordFormat" v-for="recordFormat in dataSource.record_format" data-test="search-result-format">
         {{ recordFormat }}
       </p>
     </div>
-    <p class="search-result-data" v-else>Data Formats Unknown</p>
-    <button class="button" @click="openSource" :href="dataSource.source_url">Visit Source URL</button>
-    <button class="source button" @click="showDetails">Source Details</button>
-    <p v-if="expand && dataSource.description">
+    <p class="search-result-data" data-test="search-result-format-unknown" v-else>Data Formats Unknown</p>
+    <button class="button" @click="openSource" :href="dataSource.source_url" data-test="search-result-source-button">Visit Source URL</button>
+    <button class="source button" @click="showDetails" data-test="search-result-source-details-button">Source Details</button>
+    <p v-if="expand && dataSource.description" data-test="search-result-description">
       {{ dataSource.description }}
     </p>
-    <p v-else-if="expand && !dataSource.description">No Description Available</p>
+    <p v-else-if="expand && !dataSource.description" data-test="search-result-description-unknown">No Description Available</p>
   </div>
 </template>
 
