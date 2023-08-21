23ae17e4961ea2d8fd07f9471aefb02b724b57d1
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-08-21 14:50:18 -0400

    Fixed data parsing to match new supabase package

--- resources/User.py
+++ resources/User.py
@@ -3,6 +3,7 @@ from flask_restful import Resource
 from flask import request, jsonify, make_response
 from flask_jwt_extended import create_access_token
 import uuid
+import json
 
 class User(Resource):
     def __init__(self, **kwargs):
@@ -13,15 +14,15 @@ class User(Resource):
             data = request.get_json()
             email = data.get('email')
             password = data.get('password')
-            user_data = self.supabase.table('users').select('*').eq('email', email).execute()['data']
-            user = {}
-            if len(user_data) > 0:
-                user = user_data[0]
+            user = self.supabase.table('users').select('*').eq('email', email).execute()
+            user_data = {}
+            if user:
+                user_data = user.data[0]
             else:
                 return {'error': 'no match'}
-            if check_password_hash(user['password_digest'], password):
+            if check_password_hash(user_data['password_digest'], password):
                 api_key = uuid.uuid4().hex
-                user_id = str(user['id'])
+                user_id = str(user_data['id'])
                 self.supabase.table('users').update({'api_key': api_key}).eq('id', user_id).execute()
                 return jsonify({'api_key': api_key})
         except Exception as e:
@@ -34,7 +35,10 @@ class User(Resource):
             password = data.get('password')
             password_digest = generate_password_hash(password)
             user = self.supabase.table('users').insert({"email": email, "password_digest": password_digest}).execute()
-            return user
+            user_data = {}
+            if user:
+                user_data = user.data[0]
+            return user_data
         except Exception as e:
             return {'error': e}
         
