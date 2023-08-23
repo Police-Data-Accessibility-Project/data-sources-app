f3223f768b8567417c737d468c5d04daac7ffd46
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-08-23 18:44:14 -0400

    Utilized new link table to optimize API queries through joins

--- resources/QuickSearch.py
+++ resources/QuickSearch.py
@@ -12,38 +12,16 @@ class QuickSearch(Resource):
     try:
         data_sources = {'count': 0, 'data': []}
 
-        # Query for all county_fips codes that match the county name searched
-        counties = self.supabase.table('counties').select('fips').eq('name', county).execute()
-        counties_fips = counties.data
+        data_source_matches = self.supabase.table('agency_source_link').select('data_sources(data_source_name:name, description, record_type, source_url, record_format, coverage_start, coverage_end, agency_supplied), agencies(agency_name:name, municipality, state_iso)').ilike('data_sources.name', f"%{search}%").eq('agencies.county_name', f"[\"{county}\"]").execute()
 
-        if len(counties_fips) > 0:
-            # For each county_fip code, query for all agencies within that county and add to all agency list
-            all_agencies = []
-            for county_fips in counties_fips:
-                fips = str(county_fips['fips'])
-                agencies = self.supabase.table('agencies').select('name, municipality, state_iso, airtable_uid').eq('county_fips', fips).execute()                
-                agencies_data = agencies.data
-                
-                for agency_data in agencies_data:
-                    all_agencies.append({**agency_data, "agency_name": agency_data.pop('name')})
+        filtered_data_source_matches = [data_source_match for data_source_match in data_source_matches.data if data_source_match['agencies'] is not None and data_source_match['data_sources'] is not None]
 
-            # For each agency_uid, find all agency and data source pairs that match the agency airtable_uid
-            for agency in all_agencies:
-                agency_airtable_uid = agency['airtable_uid']
-                agency_data_sources = self.supabase.table('agency_source_link').select('*').eq('agency_described_linked_uid', agency_airtable_uid).execute()
-                agency_data_sources_records = agency_data_sources.data
+        data_sources = {
+            "count": len(filtered_data_source_matches),
+            "data": [{**record["agencies"], **record['data_sources']} for record in filtered_data_source_matches]
+        }
 
-                for agency_data_sources_record in agency_data_sources_records:
-                    data_source_matches = self.supabase.table('data_sources').select('name, description, record_type, source_url, record_format, coverage_start, coverage_end, agency_supplied').ilike('name', f"%{search}%").eq('airtable_uid', agency_data_sources_record['airtable_uid']).execute()
-                    data_source_matches_data = data_source_matches.data
-
-                    for record in data_source_matches_data:
-                        data_sources['count'] += 1
-                        data_sources['data'].append({**record, **agency, "data_source_name": record.pop('name')})
-            return data_sources
-
-        else:
-            return data_sources
+        return data_sources
         
     except:
         print('Error during quick search operation')
