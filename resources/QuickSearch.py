6ecd2c33f3866b1615c3b41c4ff476bf4da23075
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-08-23 16:26:47 -0400

    Pointed all queries to new capitalized tables and updated queries to get expected data

--- resources/QuickSearch.py
+++ resources/QuickSearch.py
@@ -21,18 +21,25 @@ class QuickSearch(Resource):
             all_agencies = []
             for county_fips in counties_fips:
                 fips = str(county_fips['fips'])
-                agencies = self.supabase.table('Agencies').select('name, municipality, state_iso, airtable_uid').eq('county_fips', fips).execute()
+                agencies = self.supabase.table('Agencies').select('name, municipality, state_iso, airtable_uid').eq('county_fips', fips).execute()                
                 agencies_data = agencies.data
+                
                 for agency_data in agencies_data:
                     all_agencies.append({**agency_data, "agency_name": agency_data.pop('name')})
 
-            # For each agency_uid, find all matches in the data_sources table that also have a partial match with the search term
+            # For each agency_uid, find all agency and data source pairs that match the agency airtable_uid
             for agency in all_agencies:
-                agency_data_sources = self.supabase.table('Data Sources').select('name, description, record_type, source_url, record_format, coverage_start, coverage_end, agency_supplied').ilike('name', f"%{search}%").eq('agency_described', f"['{agency['airtable_uid']}']").execute()
+                agency_airtable_uid = agency['airtable_uid']
+                agency_data_sources = self.supabase.table('agency_source_link').select('*').eq('agency_described_linked_uid', agency_airtable_uid).execute()
                 agency_data_sources_records = agency_data_sources.data
-                for record in agency_data_sources_records:
-                    data_sources['count'] += 1
-                    data_sources['data'].append({**record, **agency, "data_source_name": record.pop('name')})
+
+                for agency_data_sources_record in agency_data_sources_records:
+                    data_source_matches = self.supabase.table('Data Sources').select('name, description, record_type, source_url, record_format, coverage_start, coverage_end, agency_supplied').ilike('name', f"%{search}%").eq('airtable_uid', agency_data_sources_record['airtable_uid']).execute()
+                    data_source_matches_data = data_source_matches.data
+
+                    for record in data_source_matches_data:
+                        data_sources['count'] += 1
+                        data_sources['data'].append({**record, **agency, "data_source_name": record.pop('name')})
             return data_sources
 
         else:
