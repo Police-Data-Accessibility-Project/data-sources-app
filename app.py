f5fd8847caa7b09855fe628ba1739d921daec74f
Marty <mbodenator@gmail.com>
2023-08-31 12:09:01 -0400

    set env vars externally

--- app.py
+++ app.py
@@ -6,26 +6,7 @@ from resources.QuickSearch import QuickSearch
 from middleware.initialize_supabase_client import initialize_supabase_client
 import os
 
-def read_env():
-    app_env = os.environ.get('APP_ENV', 'local')
-    
-    if app_env == 'local':
-        try:
-            with open('.env') as file:
-                for line in file:
-                    key, value = line.strip().split('=', 1)
-                    os.environ[key] = value
-        except FileNotFoundError:
-            print("The '.env' file was not found. Please create the file and set the necessary environment variables.")
-            data_sources = {'count': 0, 'data': []}
-            return data_sources
-        except: 
-            print('Cannot open file')
-            file.close()
-            data_sources = {'count': 0, 'data': []}
-            return data_sources
 
-read_env()
 supabase = initialize_supabase_client()
 
 app = Flask(__name__)
