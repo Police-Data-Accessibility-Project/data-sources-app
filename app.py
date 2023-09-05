e2ea2b28a5c9d44e9e8c7a061ed1f1512a1042d6
Marty <mbodenator@gmail.com>
2023-09-05 11:06:13 -0400

    clean up dependencies for deploy

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ api.add_resource(QuickSearch, '/quick-search/<search>/<county>', resource_class_
 api.add_resource(DataSources, '/data-sources', resource_class_kwargs={"supabase": supabase})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
