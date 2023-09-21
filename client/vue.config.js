694148b32a33e4a3200b1acd5cade15bee071467
Emmaline Rial <emr102@pitt.edu>
2023-09-21 15:46:07 -0400

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
 	devServer: {
-		allowedHosts: [
-			'https://data-sources-app-dev-8vdb2.ondigitalocean.app'
-		],
+		allowedHosts: 'all',
   },
 });
