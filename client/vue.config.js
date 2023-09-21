1cc02e58357250df0bcdff27ba31ba738af403d0
Emmaline Rial <emr102@pitt.edu>
2023-09-21 16:31:43 -0400

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
 	devServer: {
-		allowedHosts: 'all',
-  },
-	
+		allowedHosts: 'all' },
 });
