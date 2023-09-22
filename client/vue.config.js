db82e3a38d33b6a832ab238f394237448f23b777
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-09-22 17:39:24 -0400

    Change publicPath

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: 'client/',
+	publicPath: '/',
 	devServer: {
 		allowedHosts: 'all',
 	},
