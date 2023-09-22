4b71b8688bd148c04fa44ec29a69cd564b407aa2
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-09-22 14:42:10 -0400

    Change configuration for Vue

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: "",
+	publicPath: '',
 	devServer: {
-		allowedHosts: 'all' },
+		allowedHosts: 'all',
+	},
+	outputDir: 'dist',
 });
