e2f91414612ed58a97a7d6181833e4afebf7dba9
Emmaline Rial <emr102@pitt.edu>
2023-09-23 11:18:15 -0400

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: '/data-sources-app-client',
+	publicPath: process.env.NODE_ENV === 'production',
 	devServer: {
 		allowedHosts: 'all',
 	},
