0bd94a3d7a3c61991065862331b557abfcad3ab6
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-09-29 12:48:22 -0400

    Update config file to point to updated publicPath

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: '/data-sources-app-client',
+	publicPath: '/',
 	devServer: {
 		allowedHosts: 'all',
 	},
