c08625fc0646536b572eaac1ffacce513a31ed61
Marty <mbodenator@gmail.com>
2023-11-07 14:53:21 -0500

    county fix, tests

--- middleware/initialize_psycopg2_connection.py
+++ middleware/initialize_psycopg2_connection.py
@@ -1,11 +1,16 @@
 import psycopg2
 import os
+from decouple import config
 
 def initialize_psycopg2_connection():
     try:
         SUPABASE_DATABASE_URL = os.getenv('SUPABASE_DATABASE_URL')
+        if not SUPABASE_DATABASE_URL:
+            SUPABASE_DATABASE_URL = config('SUPABASE_DATABASE_URL')
+
         return psycopg2.connect(SUPABASE_DATABASE_URL)
     except:
         print('Error while initializing the Supabase client with psycopg2.')
         data_sources = {'count': 0, 'data': []}
+
         return data_sources
