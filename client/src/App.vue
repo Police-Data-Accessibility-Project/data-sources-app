866b95ae71cde9e8be213e5ac3ff7dd4c671a9c5
Joshua Graber <joshua.d.graber@gmail.com>
2023-11-29 11:33:29 -0500

    feature(QuickSearch): add design system components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <AppHeader />
-  <router-view></router-view>
-  <AppFooter />
+	<AppHeader />
+	<router-view></router-view>
+	<AppFooter />
 </template>
 
 <script>
@@ -9,23 +9,29 @@ import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
-  name: 'App',
-  components: {
-    AppHeader,
-    AppFooter
-  }
-}
+	name: 'App',
+	components: {
+		AppHeader,
+		AppFooter,
+	},
+};
 </script>
 
 <style>
 #app {
-  margin: 0;
-  padding: 1rem;
+	margin: 0;
+	padding: 1rem;
 }
 
 .logo {
-  width: 50px;
-  margin: 1rem;
-  cursor: pointer;
+	width: 50px;
+	margin: 1rem;
+	cursor: pointer;
+}
+
+/* Temporary code to override user agent styles
+    TODO: remove when https://github.com/Police-Data-Accessibility-Project/design-system/issues/19 is addressed and released */
+.pdap-button-primary[type='submit'] {
+	background-color: rgba(var(--color-brand-gold) / var(--tw-bg-opacity));
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import 'pdap-design-system/styles';
+
 createApp(App).use(router).mount('#app');
