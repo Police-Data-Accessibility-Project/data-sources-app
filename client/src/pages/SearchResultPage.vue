99c63affc52adcf53e1215c25ad2174cde9da7e4
Marty <mbodenator@gmail.com>
2023-12-08 11:49:29 -0500

    Revert "headers fix"
    This reverts commit 0be9a6445c2d50252ec87832920d0c0787a752f0.

--- client/src/pages/SearchResultPage.vue
+++ client/src/pages/SearchResultPage.vue
@@ -46,7 +46,8 @@ export default {
   methods: {
     async search() {
       try{
-        const res = await axios.get(`${process.env.VUE_APP_BASE_URL}/search-tokens?endpoint=quick-search&arg1=${this.searchTerm}&arg2=${this.location}`)
+        const headers = {"Authorization": `Bearer ${process.env.VUE_APP_PDAP_TOKEN}`}
+        const res = await axios.get(`${process.env.VUE_APP_BASE_URL}/search-tokens?endpoint=quick-search&arg1=${this.searchTerm}&arg2=${this.location}`, {headers})
         this.searchStatusCode = res.status
         this.searchResult = res.data
         this.searched = true
