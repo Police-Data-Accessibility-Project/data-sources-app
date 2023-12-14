015f047a226885154cf359044b95be3f612d1815
Josh Chamberlain <josh@getwhiplash.com>
2023-12-14 16:46:28 -0500

    install tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+    "src/**/*.{vue,js,css}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
