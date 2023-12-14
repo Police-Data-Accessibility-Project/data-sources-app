9772d6ac42025694674a9d976b9992dd35bf496d
Josh Chamberlain <josh@getwhiplash.com>
2023-12-14 16:35:45 -0500

    quick search help text

--- client/src/pages/QuickSearchPage.vue
+++ client/src/pages/QuickSearchPage.vue
@@ -1,14 +1,25 @@
 <template>
+	<FlexContainer
+		component="section"
+		class="h-auto p-0 mt-12"
+		>
+		<h2 class="mt-0">Search our database</h2>
+			<p class="pb-4 md:pb-8">
+				If you have a question to answer, we may already know about helpful data in your area.
+				<RouterLink to="/data">Learn more about the data here.</RouterLink>
+			</p>
+	</FlexContainer>
 	<QuickSearchForm />
 </template>
 
 <script>
-import { QuickSearchForm } from "pdap-design-system";
+import { GridContainer, QuickSearchForm } from "pdap-design-system";
 
 export default {
 	name: "QuickSearchPage",
 	components: {
-		QuickSearchForm,
-	},
+    QuickSearchForm,
+    GridContainer
+},
 };
 </script>
