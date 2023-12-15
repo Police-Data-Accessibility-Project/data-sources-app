2c5dcd1576c0f2f98507f5e2bdf3cd814805f870
Josh Chamberlain <josh@getwhiplash.com>
2023-12-15 15:46:39 -0500

    updated search form

--- client/src/pages/QuickSearchPage.vue
+++ client/src/pages/QuickSearchPage.vue
@@ -1,15 +1,7 @@
 <template>
-	<FlexContainer
-		component="section"
-		class="h-auto p-0 mt-12"
-		>
-		<h2 class="mt-0">Search our database</h2>
-			<p class="pb-4 md:pb-8">
-				If you have a question to answer, we may already know about helpful data in your area.
-				<RouterLink to="/data">Learn more about the data here.</RouterLink>
-			</p>
-	</FlexContainer>
-	<QuickSearchForm />
+	<main class="md:pb-[60vh] md:mt-24">
+		<QuickSearchForm />
+	</main>
 </template>
 
 <script>
