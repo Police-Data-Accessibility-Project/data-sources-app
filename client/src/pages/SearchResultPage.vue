e34785ab65fa1188cee6c895689630e4eec2fc24
Josh Chamberlain <josh@getwhiplash.com>
2023-12-19 11:35:51 -0500

    main results container to flex

--- client/src/pages/SearchResultPage.vue
+++ client/src/pages/SearchResultPage.vue
@@ -1,45 +1,53 @@
 <template>
-	<GridContainer
-		:columns="3"
-		templateRows="auto auto 1fr"
-		component="section"
+	<FlexContainer
+		component="main"
 		data-test="search-results-page"
 	>
-		<GridItem :span-column="3">
+		<div>
 			<h1>Search results</h1>
 			<p data-test="search-results-section-header-p" class="text-xl">
 					Searching for <span class="font-semibold">"{{ searchTerm }}"</span> 
 					in <span class="font-semibold">"{{ location }}"</span>.
-				</p>
-		</GridItem>      
-		<GridItem v-if="!searched" component="p" :span-column="3">
-			Loading results...
-		</GridItem>
-		<GridItem
-			v-else-if="searched && searchResult?.data?.length > 0"
-			:span-column="3"
-		>
-			<p class="text-xl max-w-none">
-				Found {{ typeof searchResult.count !== 'undefined' ? (searchResult.count === 0 ? '0 results' : (searchResult.count === 1 ? '1 result' : searchResult.count + ' results')) : '0 results' }}.
-				If you don't see what you need, 
-				<a href="https://airtable.com/shrbFfWk6fjzGnNsk">
-					make a request&nbsp;<i class="fa fa-external-link"></i>
-				</a>
 			</p>
-			<SearchResultCard
-				v-for="dataSource in searchResult?.data"
-				:key="dataSource.uuid"
-				data-test="search-results-cards"
-				:data-source="dataSource"
-			/>
-		</GridItem>
-		<GridItem
-			v-else
-			:span-column="3"
-			data-test="no-search-results"
-			><p>No results found.</p></GridItem
+		</div>      
+		<GridContainer
+			:columns="3"
+			templateRows="auto auto 1fr"
+			component="section"
+			data-test="search"
+			class="p-0"
 		>
-	</GridContainer>
+			<GridItem v-if="!searched" component="p" :span-column="3">
+				Loading results...
+			</GridItem>
+			<GridItem
+				v-else-if="searched && searchResult?.data?.length > 0"
+				:span-column="3"
+			>
+				<p class="text-xl max-w-none">
+					Found {{ typeof searchResult.count !== 'undefined' ? (searchResult.count === 0 ? '0 results' : (searchResult.count === 1 ? '1 result' : searchResult.count + ' results')) : '0 results' }}.
+					If you don't see what you need, 
+					<a href="https://airtable.com/shrbFfWk6fjzGnNsk">
+						make a request&nbsp;<i class="fa fa-external-link"></i>
+					</a>
+				</p>
+			</GridItem>
+			<GridItem
+				v-else
+				:span-column="3"
+				data-test="no-search-results"
+				><p>No results found.</p></GridItem
+			>
+			<GridItem>
+				<SearchResultCard
+					v-for="dataSource in searchResult?.data"
+					:key="dataSource.uuid"
+					data-test="search-results-cards"
+					:data-source="dataSource"
+				/>
+			</GridItem>
+		</GridContainer>
+	</FlexContainer>
 
 </template>
 
