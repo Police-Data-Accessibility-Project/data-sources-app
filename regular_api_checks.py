2a6ef1dda02d31555354c9b9deeadecb32b8b197
Marty Bode <martybode@gmail.com>
2024-01-10 12:09:38 -0500

    cleanup

--- regular_api_checks.py
+++ regular_api_checks.py
@@ -10,7 +10,8 @@ HEADERS = {"Authorization": f"Bearer {api_key}"}
 # quick-search
 def test_quicksearch_officer_involved_shootings_philadelphia_results():
     response = requests.get(
-        "https://data-sources.pdap.io/api/quick-search/Officer Involved Shootings/philadelphia", headers=HEADERS
+        "https://data-sources.pdap.io/api/quick-search/Officer Involved Shootings/philadelphia",
+        headers=HEADERS,
     )
 
     return len(response.json()["data"]) > 0
@@ -18,7 +19,8 @@ def test_quicksearch_officer_involved_shootings_philadelphia_results():
 
 def test_quicksearch_officer_involved_shootings_lowercase_philadelphia_results():
     response = requests.get(
-        "https://data-sources.pdap.io/api/quick-search/officer involved shootings/Philadelphia", headers=HEADERS
+        "https://data-sources.pdap.io/api/quick-search/officer involved shootings/Philadelphia",
+        headers=HEADERS,
     )
 
     return len(response.json()["data"]) > 0
@@ -26,46 +28,60 @@ def test_quicksearch_officer_involved_shootings_lowercase_philadelphia_results()
 
 def test_quicksearch_officer_involved_shootings_philadelphia_county_results():
     response = requests.get(
-        "https://data-sources.pdap.io/api/quick-search/Officer Involved Shootings/philadelphia county", headers=HEADERS
+        "https://data-sources.pdap.io/api/quick-search/Officer Involved Shootings/philadelphia county",
+        headers=HEADERS,
     )
 
     return len(response.json()["data"]) > 0
 
 
 def test_quicksearch_all_allgeheny_results():
-    response = requests.get("https://data-sources.pdap.io/api/quick-search/all/allegheny", headers=HEADERS)
+    response = requests.get(
+        "https://data-sources.pdap.io/api/quick-search/all/allegheny", headers=HEADERS
+    )
 
     return len(response.json()["data"]) > 0
 
 
 def test_quicksearch_complaints_all_results():
-    response = requests.get("https://data-sources.pdap.io/api/quick-search/complaints/all", headers=HEADERS)
+    response = requests.get(
+        "https://data-sources.pdap.io/api/quick-search/complaints/all", headers=HEADERS
+    )
 
     return len(response.json()["data"]) > 0
 
 
 def test_quicksearch_media_bulletin_pennsylvania_results():
-    response = requests.get("https://data-sources.pdap.io/api/quick-search/media bulletin/pennsylvania", headers=HEADERS)
+    response = requests.get(
+        "https://data-sources.pdap.io/api/quick-search/media bulletin/pennsylvania",
+        headers=HEADERS,
+    )
 
     return len(response.json()["data"]) > 0
 
 
-
 # data-sources
 def test_data_source_by_id():
-    response = requests.get("https://data-sources.pdap.io/api/data-sources-by-id/reczwxaH31Wf9gRjS", headers=HEADERS)
+    response = requests.get(
+        "https://data-sources.pdap.io/api/data-sources-by-id/reczwxaH31Wf9gRjS",
+        headers=HEADERS,
+    )
 
     return response.json()["data_source_id"] == "reczwxaH31Wf9gRjS"
 
 
 def test_data_sources():
-    response = requests.get("https://data-sources.pdap.io/api/data-sources", headers=HEADERS)
+    response = requests.get(
+        "https://data-sources.pdap.io/api/data-sources", headers=HEADERS
+    )
 
     return len(response.json()["data"]) > 0
 
 
 def test_data_sources_approved():
-    response = requests.get("https://data-sources.pdap.io/api/data-sources", headers=HEADERS)
+    response = requests.get(
+        "https://data-sources.pdap.io/api/data-sources", headers=HEADERS
+    )
     unapproved_url = "https://joinstatepolice.ny.gov/15-mile-run"
 
     return (
@@ -75,14 +91,19 @@ def test_data_sources_approved():
 
 
 def test_data_source_by_id_approved():
-    response = requests.get("https://data-sources.pdap.io/api/data-sources-by-id/rec013MFNfBnrTpZj", headers=HEADERS)
+    response = requests.get(
+        "https://data-sources.pdap.io/api/data-sources-by-id/rec013MFNfBnrTpZj",
+        headers=HEADERS,
+    )
 
     return response.json() == "Data source not found."
 
 
 # search-tokens
 def test_search_tokens_data_sources():
-    response = requests.get("https://data-sources.pdap.io/api/search-tokens?endpoint=data-sources")
+    response = requests.get(
+        "https://data-sources.pdap.io/api/search-tokens?endpoint=data-sources"
+    )
 
     return len(response.json()["data"]) > 0
 
@@ -106,7 +127,9 @@ def test_search_tokens_quick_search_complaints_allegheny_results():
 # user
 def test_get_user():
     response = requests.get(
-        "https://data-sources.pdap.io/api/user", headers=HEADERS, json={"email": "test2", "password": "test"}
+        "https://data-sources.pdap.io/api/user",
+        headers=HEADERS,
+        json={"email": "test2", "password": "test"},
     )
 
     return response
@@ -114,7 +137,9 @@ def test_get_user():
 
 # archives
 def test_get_archives():
-    response = requests.get("https://data-sources.pdap.io/api/archives", headers=HEADERS)
+    response = requests.get(
+        "https://data-sources.pdap.io/api/archives", headers=HEADERS
+    )
 
     return len(response.json()[0]) > 0
 
@@ -157,39 +182,47 @@ def test_put_archives_brokenasof():
 
 # agencies
 def test_agencies():
-    response = requests.get("https://data-sources.pdap.io/api/agencies/1", headers=HEADERS)
+    response = requests.get(
+        "https://data-sources.pdap.io/api/agencies/1", headers=HEADERS
+    )
 
     return len(response.json()["data"]) > 0
 
 
 def test_agencies_pagination():
-    response1 = requests.get("https://data-sources.pdap.io/api/agencies/1", headers=HEADERS)
-    response2 = requests.get("https://data-sources.pdap.io/api/agencies/2", headers=HEADERS)
+    response1 = requests.get(
+        "https://data-sources.pdap.io/api/agencies/1", headers=HEADERS
+    )
+    response2 = requests.get(
+        "https://data-sources.pdap.io/api/agencies/2", headers=HEADERS
+    )
 
     return response1 != response2
 
 
 def main():
-    tests = ["test_quicksearch_officer_involved_shootings_philadelphia_results", 
-             "test_quicksearch_officer_involved_shootings_lowercase_philadelphia_results",
-             "test_quicksearch_officer_involved_shootings_philadelphia_county_results",
-             "test_quicksearch_all_allgeheny_results",
-             "test_quicksearch_complaints_all_results",
-             "test_quicksearch_media_bulletin_pennsylvania_results",
-             "test_data_source_by_id",
-             "test_data_sources",
-             "test_data_sources_approved",
-             "test_data_source_by_id_approved",
-             "test_search_tokens_data_sources",
-             "test_search_tokens_data_source_by_id",
-             "test_search_tokens_quick_search_complaints_allegheny_results",
-             #"test_get_user",
-             "test_get_archives",
-             "test_put_archives",
-             "test_put_archives_brokenasof",
-             "test_agencies",
-             "test_agencies_pagination"]
-    
+    tests = [
+        "test_quicksearch_officer_involved_shootings_philadelphia_results",
+        "test_quicksearch_officer_involved_shootings_lowercase_philadelphia_results",
+        "test_quicksearch_officer_involved_shootings_philadelphia_county_results",
+        "test_quicksearch_all_allgeheny_results",
+        "test_quicksearch_complaints_all_results",
+        "test_quicksearch_media_bulletin_pennsylvania_results",
+        "test_data_source_by_id",
+        "test_data_sources",
+        "test_data_sources_approved",
+        "test_data_source_by_id_approved",
+        "test_search_tokens_data_sources",
+        "test_search_tokens_data_source_by_id",
+        "test_search_tokens_quick_search_complaints_allegheny_results",
+        # "test_get_user",
+        "test_get_archives",
+        "test_put_archives",
+        "test_put_archives_brokenasof",
+        "test_agencies",
+        "test_agencies_pagination",
+    ]
+
     fails = []
     for test in tests:
         print(f"Running test {test}...")
@@ -198,7 +231,7 @@ def main():
         else:
             print(f"Test {test} failed.")
             fails.append(test)
-    
+
     return fails
 
 
