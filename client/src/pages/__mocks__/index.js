00b8f5ee5989b13dbfea57a1035407bb6d52ac28
Joshua Graber <joshua.d.graber@gmail.com>
2024-01-18 08:23:40 -0500

    fix(tests): update tests for SearchResultsPage

---
+++ client/src/pages/__mocks__/index.js
@@ -0,0 +1,54 @@
+export const resultsMock = {
+	count: 3,
+	data: [
+		{
+			agency_name: "Cicero Police Department - IN",
+			agency_supplied: false,
+			airtable_uid: "rec7edsmcuVQwMMXG",
+			coverage_end: null,
+			coverage_start: "2017-01-01",
+			data_source_name: "Calls for Service for Cicero Police Department - IN",
+			description: null,
+			municipality: "Bridgeview",
+			name: "Cicero Police Department - IN",
+			record_format: "[]",
+			record_type: "Calls for Service",
+			source_url:
+				"https://cityprotect.com/agency/cc0d9a3d-50b2-4424-ae25-5699ba6eaff9",
+			state_iso: "IN",
+		},
+		{
+			agency_name: "Chicago Police Department - IL",
+			agency_supplied: true,
+			airtable_uid: "recv9fMNEQTbVarj2",
+			coverage_end: null,
+			coverage_start: "2019-01-01",
+			data_source_name: "Calls for Service for Chicago Police Department - IL",
+			description: null,
+			municipality: "Chicago",
+			name: "Chicago Police Department - IL",
+			record_format: "[]",
+			record_type: "Calls for Service",
+			source_url:
+				"https://informationportal.igchicago.org/911-calls-for-cpd-service/",
+			state_iso: "IL",
+		},
+		{
+			agency_name: "Chicago Police Department - IL",
+			agency_supplied: false,
+			airtable_uid: "recv9fMNEQTbVarj2",
+			coverage_end: null,
+			coverage_start: "2018-12-18",
+			data_source_name: "311 Calls for City of Chicago",
+			description:
+				"311 Service Requests received by the City of Chicago. This dataset includes requests created after the launch of the new 311 system on 12/18/2018 and some records from the previous system, indicated in the LEGACY\\_RECORD column.\n\nIncluded as a Data Source because in some cities 311 calls lead to police response; that does not appear to be the case in Chicago.\n",
+			municipality: "Chicago",
+			name: "Chicago Police Department - IL",
+			record_format: "['CSV', 'XML', 'RDF', 'RSS']",
+			record_type: "Calls for Service",
+			source_url:
+				"https://data.cityofchicago.org/Service-Requests/311-Service-Requests/v6vf-nfxy",
+			state_iso: "IL",
+		},
+	],
+};
--- client/src/pages/__tests__/searchResultPage.test.js
+++ client/src/pages/__tests__/searchResultPage.test.js
@@ -1,7 +1,10 @@
 import SearchResultPage from "../SearchResultPage.vue";
-import { flushPromises, shallowMount } from "@vue/test-utils";
-import { describe, expect, vi, test } from "vitest";
+// import { FlexContainer } from "pdap-design-system";
+import { flushPromises, mount } from "@vue/test-utils";
+import { describe, expect, vi, test, beforeEach, beforeAll } from "vitest";
 import axios from "axios";
+import { resultsMock } from "../__mocks__";
+import { nextTick } from "vue";
 
 vi.mock("axios");
 
@@ -12,128 +15,81 @@ const $routeMock = {
 	},
 };
 
-describe("SearchResultPage successfully renders components", () => {
-	axios.get.mockResolvedValue({
-		data: {
-			count: 3,
-			data: [
-				{
-					agency_name: "Cicero Police Department - IN",
-					agency_supplied: false,
-					airtable_uid: "rec7edsmcuVQwMMXG",
-					coverage_end: null,
-					coverage_start: "2017-01-01",
-					data_source_name:
-						"Calls for Service for Cicero Police Department - IN",
-					description: null,
-					municipality: "Bridgeview",
-					name: "Cicero Police Department - IN",
-					record_format: "[]",
-					record_type: "Calls for Service",
-					source_url:
-						"https://cityprotect.com/agency/cc0d9a3d-50b2-4424-ae25-5699ba6eaff9",
-					state_iso: "IN",
-				},
-				{
-					agency_name: "Chicago Police Department - IL",
-					agency_supplied: true,
-					airtable_uid: "recv9fMNEQTbVarj2",
-					coverage_end: null,
-					coverage_start: "2019-01-01",
-					data_source_name:
-						"Calls for Service for Chicago Police Department - IL",
-					description: null,
-					municipality: "Chicago",
-					name: "Chicago Police Department - IL",
-					record_format: "[]",
-					record_type: "Calls for Service",
-					source_url:
-						"https://informationportal.igchicago.org/911-calls-for-cpd-service/",
-					state_iso: "IL",
-				},
-				{
-					agency_name: "Chicago Police Department - IL",
-					agency_supplied: false,
-					airtable_uid: "recv9fMNEQTbVarj2",
-					coverage_end: null,
-					coverage_start: "2018-12-18",
-					data_source_name: "311 Calls for City of Chicago",
-					description:
-						"311 Service Requests received by the City of Chicago. This dataset includes requests created after the launch of the new 311 system on 12/18/2018 and some records from the previous system, indicated in the LEGACY\\_RECORD column.\n\nIncluded as a Data Source because in some cities 311 calls lead to police response; that does not appear to be the case in Chicago.\n",
-					municipality: "Chicago",
-					name: "Chicago Police Department - IL",
-					record_format: "['CSV', 'XML', 'RDF', 'RSS']",
-					record_type: "Calls for Service",
-					source_url:
-						"https://data.cityofchicago.org/Service-Requests/311-Service-Requests/v6vf-nfxy",
-					state_iso: "IL",
-				},
-			],
-		},
-	});
+let wrapper;
 
-	const wrapper = shallowMount(SearchResultPage, {
-		global: {
-			mocks: {
-				$route: $routeMock,
+beforeAll(() => {
+	import.meta.env.VITE_VUE_APP_BASE_URL = "https://data-sources.pdap.io";
+});
+
+describe("SearchResultPage renders with data", () => {
+	beforeEach(() => {
+		wrapper = mount(SearchResultPage, {
+			global: {
+				mocks: {
+					$route: $routeMock,
+				},
+				stubs: {
+					FlexContainer: {
+						template: "<main><slot /></main>",
+					},
+				},
 			},
-		},
+		});
+
+		axios.get.mockResolvedValue({
+			data: resultsMock,
+		});
 	});
 
-	test("renders search results after API call", () => {
-		expect(wrapper.find('[data-test="search-results-page"]').exists()).toBe(
-			true,
-		);
-	});
-
-	test("renders search results section after API call", () => {
-		expect(wrapper.find('[data-test="search-results-section"]').exists()).toBe(
-			true,
-		);
-	});
-
-	test("renders search results section header properly", () => {
+	test("Calls API and renders search results", async () => {
 		const searchTerm = wrapper.vm.searchTerm;
 		const location = wrapper.vm.location;
 
+		await wrapper.vm.search();
+		await flushPromises();
+
+		expect(axios.get).toHaveBeenCalledTimes(2);
+		expect(axios.get).toHaveBeenCalledWith(
+			`https://data-sources.pdap.io/search-tokens?endpoint=quick-search&arg1=${searchTerm}&arg2=${location}`,
+		);
+
+		await nextTick();
+
+		expect(wrapper.find('[data-test="search-results-page"]').exists()).toBe(
+			true,
+		);
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	test("renders search results section header properly", () => {
+		axios.get.mockResolvedValue({
+			data: resultsMock,
+		});
+
+		const searchTerm = wrapper.vm.searchTerm;
+		const location = wrapper.vm.location;
+		const count = wrapper.vm.searchResult.count;
+
 		expect(
 			wrapper.get('[data-test="search-results-section-header-p"]').text(),
-		).toBe(`Searching for "${searchTerm}" in "${location}".`);
+		).toBe(
+			`Searching for "${searchTerm}" in "${location}". Found ${count} results.`,
+		);
 	});
 
 	test("renders search result count properly", () => {
-		const count = wrapper.vm.searchResult.count;
+		// const count = wrapper.vm.searchResult.count;
 
 		expect(wrapper.get('[data-test="search-results-count"]').text()).toBe(
-			` Found ${
-				typeof count !== "undefined"
-					? count === 0
-						? "0 results"
-						: count === 1
-						  ? "1 result"
-						  : count + " results"
-					: "0 results"
-			}.`,
+			`Found ${wrapper.vm.getResultsCopy()}.`,
 		);
 	});
 
-	test("request data button opens form", () => {
-		window.open = vi.fn();
-
-		wrapper
-			.get('[data-test="search-results-section-header-button"]')
-			.trigger("click");
-
-		expect(window.open).toHaveBeenCalledWith(
-			"https://airtable.com/shrbFfWk6fjzGnNsk",
-			"_blank",
-		);
-	});
-
-	test("search results content section renders", () => {
-		expect(wrapper.find('[data-test="search-results-content"]').exists()).toBe(
-			true,
-		);
+	test("request data link has correct href value", () => {
+		expect(
+			wrapper.get('[data-test="search-results-request-link"]').attributes()
+				.href,
+		).toBe("https://airtable.com/shrbFfWk6fjzGnNsk");
 	});
 
 	test("search results card count matches search results returned in data", () => {
@@ -143,44 +99,16 @@ describe("SearchResultPage successfully renders components", () => {
 	});
 });
 
-describe("SearchResultPage shows loading screen before API call completes", () => {
-	test("loading section appears before API call completes", async () => {
-		axios.get.mockImplementation(() => {
-			return new Promise((resolve) => {
-				setTimeout(() => resolve({ data: { count: 0, data: {} } }), 100); // Simulate 1-second delay
-			});
-		});
-
-		const wrapper = shallowMount(SearchResultPage, {
-			global: {
-				mocks: {
-					$route: $routeMock,
-				},
-			},
-		});
-
-		expect(wrapper.find('[data-test="loading-section"]').exists()).toBe(true);
-
-		await wrapper.vm.search();
-
-		expect(wrapper.find('[data-test="loading-section"]').exists()).toBe(false);
-
-		expect(wrapper.find('[data-test="search-results-section"]').exists()).toBe(
-			true,
-		);
-	});
-});
-
 describe("SearchResultPage shows no results found when no data returns", () => {
 	test("search results page shows no results properly", async () => {
-		await axios.get.mockResolvedValue({
+		axios.get.mockResolvedValue({
 			data: {
 				count: 0,
 				data: [],
 			},
 		});
 
-		const wrapper = shallowMount(SearchResultPage, {
+		wrapper = mount(SearchResultPage, {
 			global: {
 				mocks: {
 					$route: $routeMock,
