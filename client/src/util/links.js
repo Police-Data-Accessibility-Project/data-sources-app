bc35a023506d999057cb1fdbac8c9c68fec114bb
Joshua Graber <joshua.d.graber@gmail.com>
2024-01-18 10:45:46 -0500

    refactor: move links to util

---
+++ client/src/util/links.js
@@ -0,0 +1,35 @@
+// Ignore coverage because this is static data
+/* c8 ignore start */
+export const links = [
+	{
+		path: "/",
+		text: "Search",
+		method: "route",
+	},
+	{
+		href: "https://pdap.io/data",
+		text: "Data",
+		method: "href",
+	},
+	{
+		href: "https://pdap.io/community",
+		text: "Community",
+		method: "href",
+	},
+	{
+		href: "https://pdap.io/about",
+		text: "About",
+		method: "href",
+	},
+	{
+		href: "https://pdap.io/donate",
+		text: "Donate",
+		method: "href",
+	},
+	{
+		href: "https://docs.pdap.io/",
+		text: "Docs",
+		method: "href",
+	},
+];
+/* c8 ignore stop */
