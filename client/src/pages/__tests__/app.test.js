006e247ca815d09df2426661080d09e371de6fb9
Joshua Graber <joshua.d.graber@gmail.com>
2024-01-18 10:54:43 -0500

    test(App): add test for top-level app component

---
+++ client/src/pages/__tests__/app.test.js
@@ -0,0 +1,32 @@
+import { mount, RouterLinkStub, RouterViewStub } from "@vue/test-utils";
+import App from "../../App.vue";
+import { beforeEach, describe, expect, it } from "vitest";
+import { links } from "../../util/links";
+
+let wrapper;
+
+describe("App", () => {
+	beforeEach(() => {
+		wrapper = mount(App, {
+			global: {
+				provide: {
+					navLinks: links,
+					footerLinks: links,
+				},
+				stubs: {
+					RouterLink: RouterLinkStub,
+					RouterView: RouterViewStub,
+				},
+			},
+		});
+	});
+
+	it("renders the App", () => {
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	it("provides the correct navLinks and footerLinks data", () => {
+		expect(wrapper.vm.$options.provide.navLinks).toEqual(links);
+		expect(wrapper.vm.$options.provide.footerLinks).toEqual(links);
+	});
+});
