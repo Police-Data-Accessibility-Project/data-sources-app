8e51b698c04c86d45649472a2f3d9776b85bf647
Marty Bode <martybode@gmail.com>
2024-01-19 12:25:37 -0500

    no results fix

--- middleware/quick_search_query.py
+++ middleware/quick_search_query.py
@@ -57,8 +57,7 @@ def unaltered_search_query(cursor, search, location):
     print(f"Query parameters: '%{search}%', '%{location}%'")
     cursor.execute(QUICK_SEARCH_SQL.format(search.title(), location.title()))
     results = cursor.fetchall()
-    cursor.close()
-
+    
     return results
 
 
@@ -77,8 +76,7 @@ def spacy_search_query(cursor, search, location):
         QUICK_SEARCH_SQL.format(depluralized_search_term.title(), location.title())
     )
     results = cursor.fetchall()
-    cursor.close()
-
+    
     return results
 
 
@@ -136,5 +134,6 @@ def quick_search_query(search="", location="", test_query_results=[], conn={}):
             ),
         )
         conn.commit()
+        cursor.close()
 
     return data_sources
