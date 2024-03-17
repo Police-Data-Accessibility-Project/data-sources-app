75f574b55a56c4caf33aec8e02364561d9fd0513
maxachis <maxachis@gmail.com>
2024-03-17 19:01:31 -0400

    Encapsulate app creation in create_app function

--- app.py
+++ app.py
@@ -18,74 +18,80 @@ from resources.Archives import Archives
 from resources.SearchTokens import SearchTokens
 from middleware.initialize_psycopg2_connection import initialize_psycopg2_connection
 
-psycopg2_connection = initialize_psycopg2_connection()
 
-app = Flask(__name__)
-api = Api(app)
-CORS(app)
+def create_app() -> Flask:
+    psycopg2_connection = initialize_psycopg2_connection()
 
-api.add_resource(
-    User, "/user", resource_class_kwargs={"psycopg2_connection": psycopg2_connection}
-)
-api.add_resource(
-    Login, "/login", resource_class_kwargs={"psycopg2_connection": psycopg2_connection}
-)
-api.add_resource(
-    RefreshSession,
-    "/refresh-session",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    ApiKey,
-    "/api_key",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    RequestResetPassword,
-    "/request-reset-password",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    ResetPassword,
-    "/reset-password",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    QuickSearch,
-    "/quick-search/<search>/<location>",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    Archives,
-    "/archives",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    DataSources,
-    "/data-sources",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    DataSourcesNeedsIdentification,
-    "/data-sources-needs-identification",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    DataSourceById,
-    "/data-sources-by-id/<data_source_id>",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    Agencies,
-    "/agencies/<page>",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
-api.add_resource(
-    SearchTokens,
-    "/search-tokens",
-    resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
-)
+    app = Flask(__name__)
+    api = Api(app)
+    CORS(app)
+
+
+    api.add_resource(
+        User, "/user", resource_class_kwargs={"psycopg2_connection": psycopg2_connection}
+    )
+    api.add_resource(
+        Login, "/login", resource_class_kwargs={"psycopg2_connection": psycopg2_connection}
+    )
+    api.add_resource(
+        RefreshSession,
+        "/refresh-session",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        ApiKey,
+        "/api_key",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        RequestResetPassword,
+        "/request-reset-password",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        ResetPassword,
+        "/reset-password",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        QuickSearch,
+        "/quick-search/<search>/<location>",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        Archives,
+        "/archives",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        DataSources,
+        "/data-sources",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        DataSourcesNeedsIdentification,
+        "/data-sources-needs-identification",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        DataSourceById,
+        "/data-sources-by-id/<data_source_id>",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        Agencies,
+        "/agencies/<page>",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+    api.add_resource(
+        SearchTokens,
+        "/search-tokens",
+        resource_class_kwargs={"psycopg2_connection": psycopg2_connection},
+    )
+
+    return app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True, host="0.0.0.0")
