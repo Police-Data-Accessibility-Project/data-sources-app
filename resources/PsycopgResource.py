41c97fe857f701364b23eb546251f78595cc7b47
maxachis <maxachis@gmail.com>
2024-04-26 18:00:37 -0400

    Update PsycopgResource to use flask-restx
    The PsycopgResource has been updated to utilize the flask-restx library instead of flask_restful. Additionally, the __init__ method has been altered to accept positional arguments and explicitly invoke the superclass's initialization method.

--- resources/PsycopgResource.py
+++ resources/PsycopgResource.py
@@ -1,12 +1,13 @@
-from flask_restful import Resource
+from flask_restx import Resource
 
 
 class PsycopgResource(Resource):
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Initializes the resource with a database connection.
         - kwargs (dict): Keyword arguments containing 'psycopg2_connection' for database connection.
         """
+        super().__init__(*args, **kwargs)
         self.psycopg2_connection = kwargs["psycopg2_connection"]
 
     def get(self):
