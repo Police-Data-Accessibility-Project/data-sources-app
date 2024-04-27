df73995720fc6cfd875f784d035cd10dc21ccda1
maxachis <maxachis@gmail.com>
2024-04-27 15:43:55 -0400

    Add user-starred data sources management
    This commit introduces a new functionality for managing user-starred data sources. It includes two new files: a middleware CRUD manager and a Flask Resource. Both are designed to handle operations on the PostgreSQL 'user_starred_data_sources' table. The changes also include an update to the app.py to provide the necessary API endpoint for this feature.

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from resources.Agencies import Agencies
 from resources.Archives import Archives
 from resources.SearchTokens import SearchTokens
 from middleware.initialize_psycopg2_connection import initialize_psycopg2_connection
+from resources.UserStarDatasource import UserStarDatasource
 
 
 def add_resource(api, resource, endpoint, **kwargs):
@@ -48,6 +49,7 @@ def create_app() -> Flask:
         (DataSourceById, "/data-sources-by-id/<data_source_id>"),
         (Agencies, "/agencies/<page>"),
         (SearchTokens, "/search-tokens"),
+        (UserStarDatasource, "/user-star"),
     ]
 
     for resource, endpoint in resources:
