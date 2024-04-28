74bbb528e42dadae62906c4b99e869c3e8024b3f
maxachis <maxachis@gmail.com>
2024-04-28 12:32:40 -0400

    Add DataRequest to routes in app.py
    The DataRequest resource is now imported and one of its routes is added to the application's URL structure. This will enable us to access DataRequest actions through HTTP requests, expanding our API functionality.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
+
+from resources.DataRequest import DataRequest
 from resources.User import User
 from resources.Login import Login
 from resources.RefreshSession import RefreshSession
@@ -48,6 +50,7 @@ def create_app() -> Flask:
         (DataSourceById, "/data-sources-by-id/<data_source_id>"),
         (Agencies, "/agencies/<page>"),
         (SearchTokens, "/search-tokens"),
+        (DataRequest, "/data-request")
     ]
 
     for resource, endpoint in resources:
