10dc54eb37fb8f71f960945a042d1d7723850fef
maxachis <maxachis@gmail.com>
2024-05-14 16:49:37 -0400

    Add test skeletons for UserNotLoggedIn exception handling

---
+++ tests/middleware/test_login_queries.py
@@ -0,0 +1,7 @@
+def test_is_admin_raises_user_not_logged_in_error():
+    """
+    Check that when searching for a user by an email that doesn't exist,
+    the UserNotFoundError is raised
+    :return:
+    """
+    raise NotImplementedError
---
+++ tests/middleware/test_security.py
@@ -0,0 +1,8 @@
+
+def test_api_required_user_not_found():
+    """
+    Test that the api_required decorator properly returns
+    the expected result when a user doesn't exist
+    :return:
+    """
+    raise NotImplementedError
