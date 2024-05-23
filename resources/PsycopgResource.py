3ac41a4ab9fcb3ef39404a64367979f262ebf657
maxachis <maxachis@gmail.com>
2024-05-23 12:01:19 -0400

    Remove GET and POST methods from PsycopgResource
    The base implementations of GET and POST methods have been removed from the PsycopgResource module. This implies that these methods should now be strictly defined in the individual subclasses and it avoids raising unnecessary NotImplementedError exceptions.

--- resources/PsycopgResource.py
+++ resources/PsycopgResource.py
@@ -49,14 +49,3 @@ class PsycopgResource(Resource):
         """
         self.psycopg2_connection = kwargs["psycopg2_connection"]
 
-    def get(self):
-        """
-        Base implementation of GET. Override in subclasses as needed.
-        """
-        raise NotImplementedError("This method should be overridden by subclasses")
-
-    def post(self):
-        """
-        Base implementation of POST. Override in subclasses as needed.
-        """
-        raise NotImplementedError("This method should be overridden by subclasses")
