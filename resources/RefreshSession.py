a4b6ef9860e9d1e48ce3a6a7e299cd7e4a2933a5
maxachis <maxachis@gmail.com>
2024-06-01 14:15:53 -0400

    Refactored RefreshSession.py to include delete_session_token function and improve session token refreshing logic

--- resources/RefreshSession.py
+++ resources/RefreshSession.py
@@ -1,6 +1,5 @@
 from flask import request
-from middleware.login_queries import token_results, create_session_token
-from datetime import datetime as dt
+from middleware.login_queries import token_results, create_session_token, delete_session_token
 from typing import Dict, Any
 
 from resources.PsycopgResource import PsycopgResource, handle_exceptions
@@ -25,17 +24,15 @@ class RefreshSession(PsycopgResource):
         old_token = data.get("session_token")
         cursor = self.psycopg2_connection.cursor()
         user_data = token_results(cursor, old_token)
-        cursor.execute(
-            f"delete from session_tokens where token = '{old_token}' and expiration_date < '{dt.utcnow()}'"
-        )
+        delete_session_token(cursor, old_token)
         self.psycopg2_connection.commit()
 
         if "id" in user_data:
             token = create_session_token(cursor, user_data["id"], user_data["email"])
-            self.psycopg2_connection.commit()
             return {
                 "message": "Successfully refreshed session token",
                 "data": token,
             }
+        self.psycopg2_connection.commit()
 
         return {"message": "Invalid session token"}, 403
