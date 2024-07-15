abb008f271235f64b96b5051675fafc8c29592c2
maxachis <maxachis@gmail.com>
2024-07-15 07:58:34 -0400

    Add ReverseProxied middleware to handle URL routing and HTTP scheme different than local environment. Fix path_info and scheme based on MY_PREFIX value.

--- app.py
+++ app.py
@@ -22,6 +22,30 @@ from resources.Archives import Archives
 from resources.SearchTokens import SearchTokens
 from middleware.initialize_psycopg2_connection import initialize_psycopg2_connection
 
+MY_PREFIX = '/api'
+
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = MY_PREFIX
+        environ['SCRIPT_NAME'] = script_name
+        path_info = environ['PATH_INFO']
+        if path_info.startswith(script_name):
+            environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
 
 def add_resource(api, resource, endpoint, **kwargs):
     api.add_resource(resource, endpoint, resource_class_kwargs=kwargs)
@@ -30,6 +54,7 @@ def add_resource(api, resource, endpoint, **kwargs):
 def create_app() -> Flask:
     psycopg2_connection = initialize_psycopg2_connection()
     app = Flask(__name__)
+    app.wsgi_app = ReverseProxied(app.wsgi_app)
     api = Api(app)
     CORS(app)
 
