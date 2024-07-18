a2876dd22a7e82dc62e5fcf549ba5d3bbac3730a
maxachis <maxachis@gmail.com>
2024-07-18 07:45:00 -0400

    Refactor namespace creation to accept specific namespace attributes

--- utilities/namespace.py
+++ utilities/namespace.py
@@ -1,13 +1,25 @@
 from flask_restx import Namespace
+from enum import Enum
+from collections import namedtuple
 
-def create_namespace() -> Namespace:
+NamespaceAttributes = namedtuple("NamespaceAttributes", ["path", "description"])
+
+
+class AppNamespaces(Enum):
+    DEFAULT = NamespaceAttributes(path="/", description="Default Namespace")
+    SEARCH = NamespaceAttributes(path="search", description="Search Namespace")
+
+
+def create_namespace(namespace_attributes: AppNamespaces = AppNamespaces.DEFAULT) -> Namespace:
     """
-    Create a default namespace to be used with Flask_restx resources.
+    Create a namespace to be used with Flask_restx resources.
     Each namespace can contain the route definitions and other documentation about the resource
     which can then be imported into the API in the create_app function.
     :return:
     """
 
-    namespace = Namespace("/", description="Default Namespace")
+    path = namespace_attributes.value.path
+    description = namespace_attributes.value.description
+    namespace = Namespace(path, description=description)
 
     return namespace
