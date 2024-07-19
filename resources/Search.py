da365370149aad0b14611478556e38d366ccd493
maxachis <maxachis@gmail.com>
2024-07-18 20:56:42 -0400

    Added search functionality for location and record type parameters in the Search resource. Updated request parser with new arguments for state, county, locality, and record category. Updated response codes and added documentation for record types.

--- resources/Search.py
+++ resources/Search.py
@@ -3,9 +3,52 @@ from flask import Response, request
 from middleware.search_logic import search_wrapper
 from middleware.security import api_required
 from resources.PsycopgResource import PsycopgResource
+from resources.resource_helpers import add_api_key_header_arg, create_search_model
 from utilities.enums import RecordCategories
+from utilities.namespace import create_namespace, AppNamespaces
+
+namespace_search = create_namespace(namespace_attributes=AppNamespaces.SEARCH)
+
+request_parser = namespace_search.parser()
+add_api_key_header_arg(request_parser)
+request_parser.add_argument(
+    "state",
+    type=str,
+    location="args",
+    required=True,
+    help="The state of the search. Must be an exact match.",
+)
+
+request_parser.add_argument(
+    "county",
+    type=str,
+    location="args",
+    required=False,
+    help="The county of the search. If empty, all counties for the given state will be searched. Must be an exact "
+         "match.",
+)
+
+request_parser.add_argument(
+    "locality",
+    type=str,
+    location="args",
+    required=False,
+    help="The locality of the search. If empty, all localities for the given county will be searched. Must be an "
+         "exact match.",
+)
+
+request_parser.add_argument(
+    "record_category",
+    type=str,
+    location="args",
+    required=False,
+    help="The record category of the search. If empty, all categories will be searched. Must be an exact match.",
+)
+
+search_model = create_search_model(namespace_search)
 
 
+@namespace_search.route("/search-location-and-record-type")
 class Search(PsycopgResource):
     """
     Provides a resource for performing searches in the database for data sources
@@ -13,15 +56,26 @@ class Search(PsycopgResource):
     """
 
     @api_required
+    @namespace_search.expect(request_parser)
+    @namespace_search.response(200, "Success", search_model)
+    @namespace_search.response(500, "Internal server error")
+    @namespace_search.response(400, "Bad request; missing or bad API key")
+    @namespace_search.response(403, "Forbidden; invalid API key")
     def get(self) -> Response:
         """
-        Performs a search using the provided search terms and location. It attempts to find relevant
-        data sources in the database. If no results are found initially, it re-initializes the database
-        connection and tries again.
+        Performs a search using the provided search terms and location.
 
-        Parameters:
-        - search (str): The search term provided by the user.
-        - location (str): The location provided by the user.
+        Performs a search using the provided record type and location parameters.
+        It attempts to find relevant data sources in the database.
+
+        Record Types:
+        - "Police & Public Interactions"
+        - "Info about Officers"
+        - "Info about Agencies"
+        - "Agency-published Resources"
+        - "Jails & Courts"
+
+        Source of truth for record types can be found at https://app.gitbook.com/o/-MXypK5ySzExtEzQU6se/s/-MXyolqTg_voOhFyAcr-/activities/data-dictionaries/record-types-taxonomy
 
         Returns:
         - A dictionary containing a message about the search results and the data found, if any.
@@ -41,6 +95,6 @@ class Search(PsycopgResource):
                 record_category=record_category,
                 state=state,
                 county=county,
-                locality=locality
+                locality=locality,
             )
         return response
