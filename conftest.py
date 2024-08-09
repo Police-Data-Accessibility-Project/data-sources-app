e05327e36651bb9907102d090877eea174f44754
Kylie <github.a5cee@kyscott.aleeas.com>
2024-08-09 17:04:24 -0600

    Remove unnecessary pieces from conftest

--- conftest.py
+++ conftest.py
@@ -7,22 +7,13 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from middleware.models import db
 
 from app import create_app
-#from middleware.util import get_env_variable
-
-# Load environment variables
-dotenv.load_dotenv()
 
 
 @pytest.fixture(scope="module")
 def test_client():
     app = create_app()
-    '''app.config["SQLALCHEMY_DATABASE_URI"] = get_env_variable(
-        "DEV_DB_CONN_STRING"
-    )  # Connect to pre-existing test database'''
     app.config["TESTING"] = True
     app.config["SQLALCHEMY_ECHO"] = True
-
-    #db.init_app(app)
 
     with app.test_client() as testing_client:
         with app.app_context():
