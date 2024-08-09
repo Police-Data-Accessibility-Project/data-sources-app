914788f4ec3480b03e1972cf9d569039efd2a209
Kylie <github.a5cee@kyscott.aleeas.com>
2024-08-09 16:57:29 -0600

    Add initialize_sqlalchemy_session.py

--- middleware/initialize_sqlalchemy_session.py
+++ middleware/initialize_sqlalchemy_session.py
@@ -3,19 +3,29 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from middleware.models import db
 
 
-def initialize_sqlalchemy_session():
-    connection = db.engine.connect()
-    transaction = connection.begin()
-    session = scoped_session(sessionmaker(bind=connection))
+class SQLAlchemySession():
 
-    # Overwrite the db.session with the scoped session
-    db.session = session
+    def __init__(self):
+        self.session = None
+        self.initialize_sqlalchemy_session()
+
+    def initialize_sqlalchemy_session(self):
+        self.connection = db.engine.connect()
+        self.transaction = self.connection.begin()
+        self.session = scoped_session(sessionmaker(bind=self.connection))
+
+        # Overwrite the db.session with the scoped session
+        db.session = self.session
+
+    def commit_and_close(self):
+        self.session.commit()
+        self.close()
     
-    try:
-        yield session
-    except Exception as e:
-        transaction.rollback()
-        raise e
-    finally:
-        session.close()
-        connection.close()
+    def close(self):
+        self.session.close()
+        self.connection.close()
+
+    def rollback_and_close(self):
+        self.session.close()
+        self.transaction.rollback()
+        self.connection.close()
