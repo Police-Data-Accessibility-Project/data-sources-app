619fe1360d97da9f856bbada935aacce377f4273
Kylie <github.a5cee@kyscott.aleeas.com>
2024-08-11 19:48:52 -0600

    Update test_api_key_get

--- tests/integration/test_api_key.py
+++ tests/integration/test_api_key.py
@@ -4,12 +4,13 @@ from http import HTTPStatus
 
 import psycopg2.extensions
 
+from conftest import test_client, session
 from database_client.database_client import DatabaseClient
 from tests.fixtures import dev_db_connection, client_with_db
 from tests.helper_scripts.helper_functions import create_test_user_api, check_response_status
 
 
-def test_api_key_get(client_with_db, dev_db_connection: psycopg2.extensions.connection):
+def test_api_key_get(client_with_db, dev_db_connection: psycopg2.extensions.connection, test_client, session):
     """
     Test that GET call to /api_key endpoint successfully creates an API key and aligns it with the user's API key in the database
     """
@@ -24,7 +25,7 @@ def test_api_key_get(client_with_db, dev_db_connection: psycopg2.extensions.conn
 
     # Check that API key aligned with user
     cursor = dev_db_connection.cursor()
-    db_client = DatabaseClient(cursor)
+    db_client = DatabaseClient(cursor, session)
     new_user_info = db_client.get_user_info(user_info.email)
     assert new_user_info.api_key == response.json.get(
         "api_key"
