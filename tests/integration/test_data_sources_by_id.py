71df54f2be7d5db4d571d632cbef2b23901b7b2a
Kylie <github.a5cee@kyscott.aleeas.com>
2024-08-13 13:20:49 -0600

    Update test_data_sources_by_id

--- tests/integration/test_data_sources_by_id.py
+++ tests/integration/test_data_sources_by_id.py
@@ -5,6 +5,7 @@ import uuid
 import psycopg2
 from psycopg2.extras import DictCursor
 
+from conftest import test_client, session
 from database_client.database_client import DatabaseClient
 from tests.fixtures import connection_with_test_data, client_with_db, dev_db_connection
 from tests.helper_scripts.helper_functions import (
@@ -33,13 +34,13 @@ def test_data_sources_by_id_get(
 
 
 def test_data_sources_by_id_put(
-    client_with_db, connection_with_test_data: psycopg2.extensions.connection
+    client_with_db, connection_with_test_data: psycopg2.extensions.connection, test_client, session
 ):
     """
     Test that PUT call to /data-sources-by-id/<data_source_id> endpoint successfully updates the description of the data source and verifies the change in the database
     """
     tus = create_test_user_setup(client_with_db)
-    give_user_admin_role(connection_with_test_data, tus.user_info)
+    give_user_admin_role(session, tus.user_info)
     desc = str(uuid.uuid4())
     response = client_with_db.put(
         f"/api/data-sources-by-id/SOURCE_UID_1",
@@ -48,6 +49,6 @@ def test_data_sources_by_id_put(
     )
     assert response.status_code == HTTPStatus.OK.value
     cursor = connection_with_test_data.cursor(cursor_factory=DictCursor)
-    db_client = DatabaseClient(cursor)
+    db_client = DatabaseClient(cursor, session)
     result = db_client.get_data_source_by_id("SOURCE_UID_1")
     assert result["description"] == desc
