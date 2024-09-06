518478983a4f03d838daa2adbb18beee9688c670
Kylie <github.a5cee@kyscott.aleeas.com>
2024-09-06 16:57:22 -0600

    Add Agency to models

--- middleware/models.py
+++ middleware/models.py
@@ -9,16 +9,53 @@ from sqlalchemy import (
     ForeignKey,
     Enum,
 )
+from sqlalchemy.dialects.postgresql import DATE, TIMESTAMP
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.dialects.postgresql import TIMESTAMP
 
-db = SQLAlchemy()
 
 ExternalAccountType = Literal["github"]
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class Agency(Base):
+    __tablename__ = "agencies"
+    __table_args__ = {"schema": "public"}
+
+    airtable_uid: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    submitted_name: Mapped[Optional[str]]
+    homepage_url: Mapped[Optional[str]]
+    jurisdiction_type: Mapped[Optional[str]]
+    state_iso: Mapped[Optional[str]]
+    municipality: Mapped[Optional[str]]
+    county_fips: Mapped[Optional[str]] = mapped_column(
+        ForeignKey("public.counties.fips")
+    )
+    county_name: Mapped[Optional[str]]
+    lat: Mapped[Optional[float]]
+    lng: Mapped[Optional[float]]
+    defunct_year: Mapped[Optional[str]]
+    count_data_sources: Mapped[Optional[int]]
+    agency_type: Mapped[Optional[str]]
+    multi_agency: Mapped[Optional[bool]]
+    zip_code: Mapped[Optional[str]]
+    data_sources: Mapped[Optional[str]]
+    no_web_presence: Mapped[Optional[bool]]
+    airtable_agency_last_modified: Mapped[Optional[TIMESTAMP]] = mapped_column(
+        TIMESTAMP(timezone=True)
+    )
+    data_sources_last_updated: Mapped[Optional[DATE]] = mapped_column(DATE)
+    approved: Mapped[Optional[bool]]
+    rejection_reason: Mapped[Optional[str]]
+    last_approval_editor: Mapped[Optional[str]]
+    submitter_contact: Mapped[Optional[str]]
+    agency_created: Mapped[Optional[TIMESTAMP]] = mapped_column(
+        TIMESTAMP(timezone=True)
+    )
+    county_airtable_uid: Mapped[Optional[str]]
 
 
 class ExternalAccount(Base):
@@ -40,7 +77,7 @@ class TestTable(Base):
     __tablename__ = "test_table"
     __table_args__ = {"schema": "public"}
 
-    id: Mapped[BigInteger] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     pet_name: Mapped[Optional[str]] = mapped_column(String(255))
     species: Mapped[Optional[str]] = mapped_column(String(255))
 
@@ -49,9 +86,7 @@ class User(Base):
     __tablename__ = "users"
     __table_args__ = {"schema": "public"}
 
-    id: Mapped[BigInteger] = mapped_column(
-        BigInteger, primary_key=True, autoincrement=True
-    )
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     created_at: Mapped[Optional[TIMESTAMP]] = mapped_column(
         TIMESTAMP(timezone=True), server_default=text("now()")
     )
@@ -60,6 +95,7 @@ class User(Base):
     )
     email: Mapped[Text] = mapped_column(Text, unique=True)
     password_digest: Mapped[Optional[Text]] = mapped_column(Text)
-    api_key: Mapped[Optional[str]] = mapped_column(server_default=text("generate_api_key()"))
+    api_key: Mapped[Optional[str]] = mapped_column(
+        server_default=text("generate_api_key()")
+    )
     role: Mapped[Optional[Text]] = mapped_column(Text)
-
