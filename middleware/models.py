00be6afe634d990835fb2ab4522e9d652b438958
Kylie <github.a5cee@kyscott.aleeas.com>
2024-09-06 14:41:37 -0600

    Add TestTable to models

--- middleware/models.py
+++ middleware/models.py
@@ -21,6 +21,30 @@ class Base(DeclarativeBase):
     pass
 
 
+class ExternalAccount(Base):
+    __tablename__ = "external_accounts"
+    __table_args__ = {"schema": "public"}
+
+    row_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("public.users.id"))
+    account_type: Mapped[ExternalAccountType] = mapped_column(
+        Enum(*get_args(ExternalAccountType)), name="account_type"
+    )
+    account_identifier: Mapped[str] = mapped_column(String(255))
+    linked_at: Mapped[Optional[TIMESTAMP]] = mapped_column(
+        TIMESTAMP, server_default=text("now()")
+    )
+
+
+class TestTable(Base):
+    __tablename__ = "test_table"
+    __table_args__ = {"schema": "public"}
+
+    id: Mapped[BigInteger] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    pet_name: Mapped[Optional[str]] = mapped_column(String(255))
+    species: Mapped[Optional[str]] = mapped_column(String(255))
+
+
 class User(Base):
     __tablename__ = "users"
     __table_args__ = {"schema": "public"}
@@ -36,20 +60,6 @@ class User(Base):
     )
     email: Mapped[Text] = mapped_column(Text, unique=True)
     password_digest: Mapped[Optional[Text]] = mapped_column(Text)
-    api_key: Mapped[Optional[str]]
+    api_key: Mapped[Optional[str]] = mapped_column(server_default=text("generate_api_key()"))
     role: Mapped[Optional[Text]] = mapped_column(Text)
 
-
-class ExternalAccount(Base):
-    __tablename__ = "external_accounts"
-    __table_args__ = {"schema": "public"}
-
-    row_id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("public.users.id"))
-    account_type: Mapped[ExternalAccountType] = mapped_column(
-        Enum(*get_args(ExternalAccountType)), name="account_type"
-    )
-    account_identifier: Mapped[str] = mapped_column(String(255))
-    linked_at: Mapped[Optional[TIMESTAMP]] = mapped_column(
-        TIMESTAMP, server_default=text("now()")
-    )
