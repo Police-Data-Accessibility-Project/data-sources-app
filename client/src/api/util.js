8d55b801e69a67aeed95e5dd297f9d4a466cc8b9
Joshua Graber <joshua.d.graber@gmail.com>
2025-02-05 17:27:37 -0500

    chore: remove client app also removes or amends references to client in docs

--- client/src/api/util.js
+++
@@ -1,9 +0,0 @@
-export function isCachedResponseValid({
-	cacheTime,
-	intervalBeforeInvalidation = 1000 * 60 * 2,
-	currentTime = new Date().getTime(),
-}) {
-	const cacheAge = currentTime - (cacheTime ?? 0);
-
-	return cacheAge < intervalBeforeInvalidation;
-}
--- client/src/util/errors.js
+++
@@ -1,6 +0,0 @@
-export class DataLoaderErrorPassThrough extends Error {
-	constructor(message) {
-		super(message);
-		this.name = 'DataLoaderErrorPassThrough';
-	}
-}
