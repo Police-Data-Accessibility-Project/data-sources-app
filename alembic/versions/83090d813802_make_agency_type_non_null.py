1c53e34268a5df33d6a57060b228c7462b1f087b
Max Chis <maxachis@gmail.com>
2025-02-12 17:52:11 -0500

    Reformat with black

--- alembic/versions/83090d813802_make_agency_type_non_null.py
+++ alembic/versions/83090d813802_make_agency_type_non_null.py
@@ -5,6 +5,7 @@ Revises: 94da2c95b58d
 Create Date: 2025-02-11 10:05:49.486152
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,17 +13,17 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '83090d813802'
-down_revision: Union[str, None] = '94da2c95b58d'
+revision: str = "83090d813802"
+down_revision: Union[str, None] = "94da2c95b58d"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     # Add non-null constraint to agency_type
-    op.alter_column('agencies', 'agency_type', nullable=False)
+    op.alter_column("agencies", "agency_type", nullable=False)
 
 
 def downgrade() -> None:
     # Remove non-null constraint from agency_type
-    op.alter_column('agencies', 'agency_type', nullable=True)
+    op.alter_column("agencies", "agency_type", nullable=True)
