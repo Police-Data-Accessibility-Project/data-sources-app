dd342f1193242008053807273dece6140f09d975
maxachis <maxachis@gmail.com>
2025-05-09 16:25:52 -0400

    feat(database): Add data_requests to change_log tracking

--- alembic/versions/2025_05_09_1624-f4773c5636a8_add_data_request_to_change_log.py
+++ alembic/versions/2025_05_09_1624-f4773c5636a8_add_data_request_to_change_log.py
@@ -5,6 +5,7 @@ Revises: 7b0b4eaa9764
 Create Date: 2025-05-09 16:24:27.577869
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'f4773c5636a8'
-down_revision: Union[str, None] = '7b0b4eaa9764'
+revision: str = "f4773c5636a8"
+down_revision: Union[str, None] = "7b0b4eaa9764"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
