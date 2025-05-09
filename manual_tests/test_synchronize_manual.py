32c983f3df6db78a1a6485d2780d630c769929f9
maxachis <maxachis@gmail.com>
2025-05-09 16:52:27 -0400

    fix(app): fix bug in GitHub Issue relating to double quotes

--- manual_tests/test_synchronize_manual.py
+++ manual_tests/test_synchronize_manual.py
@@ -2,9 +2,13 @@ from database_client.database_client import DatabaseClient
 from middleware.primary_resource_logic.github_issue_app_logic import (
     synchronize_github_issues_with_data_requests,
 )
+from middleware.util import get_env_variable
 
 
-def test_synchronize_manual():
+def test_synchronize_manual(monkeypatch):
+    monkeypatch.setenv(
+        "DO_DATABASE_URL", get_env_variable("PROD_DATABASE_URL")
+    )
 
     # Automatically starts the Synchronize Process
     synchronize_github_issues_with_data_requests(
