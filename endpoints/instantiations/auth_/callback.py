bd7c08f0df9d78a7c4af48ef3888e71108f3ed95
Max Chis <maxachis@gmail.com>
2025-06-20 19:23:16 -0400

    Correct bug in callback endpoint not being picked up.

--- endpoints/instantiations/auth_/callback.py
+++ endpoints/instantiations/auth_/callback.py
@@ -5,12 +5,12 @@ from middleware.primary_resource_logic.callback import (
 from endpoints.psycopg_resource import PsycopgResource
 from utilities.namespace import create_namespace, AppNamespaces
 
-namespace_auth = create_namespace(AppNamespaces.AUTH)
+namespace_callback = create_namespace(AppNamespaces.AUTH)
 
 
-@namespace_auth.route("/callback")
+@namespace_callback.route("/callback")
 class Callback(PsycopgResource):
-    @namespace_auth.doc(
+    @namespace_callback.doc(
         description="""
             Callback function for multiple endpoints.
             Designed to be called by the Github OAuth2 callback function
