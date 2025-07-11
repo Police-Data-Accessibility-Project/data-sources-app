85005852f18855c5dfee52b18f54c1814db9218c
Max Chis <maxachis@gmail.com>
2025-07-10 21:32:46 -0400

    Clean up unused content

--- endpoints/instantiations/data_requests_/post/schema.py
+++ endpoints/instantiations/data_requests_/post/schema.py
@@ -1,44 +1,4 @@
-from marshmallow import Schema, fields, post_load
-
-from endpoints.instantiations.data_requests_._shared.schemas.base import DataRequestsSchema
-from endpoints.instantiations.data_requests_.post.dto import RequestInfoPostDTO, DataRequestsPostDTO
+from endpoints.instantiations.data_requests_.post.dto import DataRequestsPostDTO
 from middleware.schema_and_dto.dynamic.pydantic_to_marshmallow.core import pydantic_to_marshmallow
-from middleware.schema_and_dto.util import get_json_metadata
-
 
 DataRequestsPostSchema = pydantic_to_marshmallow(DataRequestsPostDTO)
-#
-# class DataRequestsPostSchema(Schema):
-#     request_info = fields.Nested(
-#         nested=DataRequestsSchema(
-#             only=[
-#                 "title",
-#                 "submission_notes",
-#                 "data_requirements",
-#                 "request_urgency",
-#                 "coverage_range",
-#             ]
-#         ),
-#         metadata=get_json_metadata(
-#             "The information about the data request to be created",
-#             nested_dto_class=RequestInfoPostDTO,
-#         ),
-#         required=True,
-#     )
-#     location_ids = fields.List(
-#         fields.Integer(
-#             metadata=get_json_metadata(
-#                 "The location ids associated with the data request",
-#             ),
-#         ),
-#         required=False,
-#         allow_none=True,
-#         metadata=get_json_metadata("The location ids associated with the data request"),
-#     )
-#
-#     @post_load
-#     def location_ids_convert_empty_list_to_none(self, in_data, **kwargs):
-#         location_ids = in_data.get("location_ids", None)
-#         if location_ids == []:
-#             in_data["location_ids"] = None
-#         return in_data
--- middleware/schema_and_dto/schemas/search/follow.py
+++ middleware/schema_and_dto/schemas/search/follow.py
@@ -7,40 +7,6 @@ from middleware.schema_and_dto.schemas.schema_helpers import (
 )
 from middleware.schema_and_dto.util import get_json_metadata
 
-
-class FollowSearchResponseSchema(Schema):
-    state_name = fields.Str(
-        required=True,
-        allow_none=True,
-        metadata=get_json_metadata("The state of the search."),
-    )
-    display_name = fields.Str(
-        required=True,
-        metadata=get_json_metadata("The display name of the search."),
-    )
-    county_name = fields.Str(
-        required=False,
-        allow_none=True,
-        metadata=get_json_metadata(
-            "The county of the search. If empty, all counties for the given state will be searched."
-        ),
-    )
-    locality_name = fields.Str(
-        required=False,
-        allow_none=True,
-        metadata=get_json_metadata(
-            "The locality of the search. If empty, all localities for the given county will be searched."
-        ),
-    )
-    location_id = fields.Int(
-        required=True,
-        metadata=get_json_metadata("The location ID of the search."),
-    )
-    subscriptions_by_category = fields.Dict(
-        required=True,
-        metadata=get_json_metadata("The record categories of the search."),
-    )
-
 FollowSearchResponseSchema = pydantic_to_marshmallow(FollowSearchResponseDTO)
 
 GetUserFollowedSearchesSchema = create_get_many_schema(
