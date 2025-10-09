591f1b9876a716a53794f72f6c2b014cda077dbf
Max Chis <maxachis@gmail.com>
2025-10-09 17:02:08 -0400

    Fix bug for coverage start, coverage end, and municipality type validation

--- endpoints/instantiations/search/core/models/response.py
+++ endpoints/instantiations/search/core/models/response.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import Optional
 
 from pydantic import BaseModel
@@ -9,8 +10,8 @@ from middleware.schema_and_dto.dtos._helpers import default_field_required
 class SearchResultDTO(BaseModel):
     id: int = default_field_required("The ID of the search result.")
     agency_name: str = default_field_required("The name of the agency.")
-    municipality: str = default_field_required("The municipality of the agency.")
-    state_iso: str = default_field_required("The state iso of the agency.")
+    municipality: Optional[str] = default_field_required("The municipality of the agency.")
+    state_iso: Optional[str] = default_field_required("The state iso of the agency.")
     data_source_name: str = default_field_required("The name of the data source.")
     description: Optional[str] = default_field_required(
         "The description of the data source."
@@ -20,8 +21,10 @@ class SearchResultDTO(BaseModel):
     record_formats: Optional[list[str]] = default_field_required(
         "The formats of the record."
     )
-    coverage_start: Optional[str] = default_field_required("The start of the coverage.")
-    coverage_end: Optional[str] = default_field_required("The end of the coverage.")
+    coverage_start: Optional[date] = default_field_required(
+        "The start of the coverage."
+    )
+    coverage_end: Optional[date] = default_field_required("The end of the coverage.")
     agency_supplied: bool = default_field_required(
         "Whether the agency supplied the data."
     )
