68f25ec6ecfb12db435b7efe5e4e9c80d7c4d246
Max Chis <maxachis@gmail.com>
2025-11-12 19:21:13 -0500

    Add additional test for minimal data source post

---
+++ tests/integration/data_sources/post/test_minimal.py
@@ -0,0 +1,22 @@
+from endpoints.instantiations.data_sources_.post.request_.endpoint_schema_config import \
+    PostDataSourceRequestEndpointSchemaConfig
+from endpoints.instantiations.data_sources_.post.request_.model import PostDataSourceOuterRequest, PostDataSourceRequest
+from tests.helpers.helper_classes.test_data_creator.flask import TestDataCreatorFlask
+
+
+def test_post_data_source(
+    test_data_creator_flask: TestDataCreatorFlask, agency_id_1: int, agency_id_2: int
+):
+    test_data_creator_flask.request_validator.post(
+        endpoint="/data-sources",
+        headers=test_data_creator_flask.standard_user().jwt_authorization_header,
+        json=PostDataSourceOuterRequest(
+            entry_data=PostDataSourceRequest(
+                source_url="https://www.example.com/",
+                name="test",
+                description="Test description",
+            ),
+            linked_agency_ids=[],
+        ).model_dump(mode="json"),
+        expected_schema=PostDataSourceRequestEndpointSchemaConfig.primary_output_schema,
+    )
