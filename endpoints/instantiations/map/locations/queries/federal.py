e9c6f5f3630f3edd6864e0084327b24523b97659
Max Chis <maxachis@gmail.com>
2025-11-20 11:02:42 -0500

    Rename federal query

--- endpoints/instantiations/map/locations/queries/federal.py
+++ endpoints/instantiations/map/locations/queries/federal.py
@@ -5,8 +5,8 @@ select
     data_sources.source_url,
     data_sources.id as source_id
 
-from link_agencies_data_sources
-join data_sources on link_agencies_data_sources.data_source_id = data_sources.id
-join agencies on link_agencies_data_sources.agency_id = agencies.id
+from link_agencies__data_sources
+join data_sources on link_agencies__data_sources.data_source_id = data_sources.id
+join agencies on link_agencies__data_sources.agency_id = agencies.id
 where agencies.jurisdiction_type = 'federal'
 """
